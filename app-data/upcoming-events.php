ca1f4a7fad65afb4a5543e77c9d566c004830e59
Geoffrey Roberts <groberts314@yahoo.com>
2019-03-31 08:08:07 -0700

    Add more upcoming events data to new data source file

--- app-data/upcoming-events.php
+++ app-data/upcoming-events.php
@@ -46,6 +46,151 @@ EVENT1
   <div class="event-time">6:45 PM</div>
 </div>
 EVENT2
+    ),
+    3 => array(
+      'date' => '2019-04-07',
+      'content' => <<<EVENT3
+<div class="event-detail">
+  <div class="event-description">
+    <a href="event-fliers/2019-04-07-preschool-appreciation-sunday.jpg" target="_blank">
+      <strong>Preschool Appreciation Sunday</strong>
+    </a>
+  </div>
+  <div class="event-time">10:45 AM</div>
+</div>
+EVENT3
+    ),
+    4 => array(
+      'date' => '2019-04-10',
+      'content' => <<<EVENT4
+<div class="event-detail">
+  <div class="event-description">
+    <a href="event-fliers/2019-03-06-ash-wednesday.jpg" target="_blank">
+      <strong>Lenten Midweek Morning Service</strong>
+    </a>
+  </div>
+  <div class="event-time">10:15 AM</div>
+</div>
+EVENT4
+    ),
+    5 => array(
+      'date' => '2019-04-11',
+      'content' => <<<EVENT5
+<div class="event-detail">
+  <div class="event-description">
+    <a href="event-fliers/2019-03-06-ash-wednesday.jpg" target="_blank">
+      <strong>Lenten Midweek Evening Service</strong>
+    </a>
+  </div>
+  <div class="event-time">6:45 PM</div>
+</div>
+EVENT5
+    ),
+    6 => array(
+      'date' => '2019-04-14',
+      'content' => <<<EVENT6
+<div class="event-detail">
+  <div class="event-description">
+    <a href="event-fliers/2019-04-14-holy-week-schedule.jpg" target="_blank">
+      <strong>Palm Sunday</strong>
+    </a>
+    <br />
+    Procession with Palms at 9 and 10:45 AM worship services.
+  </div>
+  <div class="event-time">9 AM</div>
+</div>
+EVENT6
+    ),
+    7 => array(
+      'date' => '2019-04-18',
+      'content' => <<<EVENT7
+<div class="event-detail">
+  <div class="event-description">
+    <a href="event-fliers/2019-04-14-holy-week-schedule.jpg" target="_blank">
+      <strong>Maundy Thursday</strong>
+    </a>
+    <br />
+    Evening Worship
+  </div>
+  <div class="event-time">7 PM</div>
+</div>
+EVENT7
+    ),
+    8 => array(
+      'date' => '2019-04-19',
+      'content' => <<<EVENT8
+<div class="event-detail">
+  <div class="event-description">
+    <a href="event-fliers/2019-04-14-holy-week-schedule.jpg" target="_blank">
+      <strong>Good Friday</strong>
+    </a>
+    <br />
+    Evening Worship
+  </div>
+  <div class="event-time">7 PM</div>
+</div>
+EVENT8
+    ),
+    9 => array(
+      'date' => '2019-04-21',
+      'content' => <<<EVENT9
+<div class="event-detail">
+  <div class="event-description">
+    <a href="event-fliers/2019-04-14-holy-week-schedule.jpg" target="_blank">
+      <strong>Easter Sunday</strong>
+    </a>
+    <br />
+    The Resurrection of our Lord!  ALLELUIA!
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    Sunrise Service
+  </div>
+  <div class="event-time">6:30 AM</div>
+ </div>
+<div class="event-detail">
+  <div class="event-description">
+    Traditional Service
+  </div>
+  <div class="event-time">9 AM</div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    Praise Service
+  </div>
+  <div class="event-time">10:45 AM</div>
+</div>
+EVENT9
+    ),
+    10 => array(
+      'date' => '2019-04-27',
+      'content' => <<<EVENT10
+<div class="event-detail">
+  <div class="event-description">
+    <a href="event-fliers/2019-04-27-synesthesia-concert.jpg" target="_blank">
+      <strong>&ldquo;American Landscapes&rdquo; Concert</strong>
+    </a>
+    <br />
+    by Synesthesia Sinfonietta
+  </div>
+  <div class="event-time">7 PM</div>
+</div>
+EVENT10
+    ),
+    11 => array(
+      'date' => '2019-04-29',
+      'content' => <<<EVENT11
+<div class="event-detail">
+  <div class="event-description">
+    <a href="event-fliers/2019-04-29-black-bear-diner-fundraiser.jpg" target="_blank">
+      <strong>&ldquo;Dine and Donate&rdquo; Fundraiser</strong>
+    </a>
+    at the <strong>Black Bear Diner</strong> in La Habra.<br />
+    <strong>4 PM - 8 PM</strong>
+  </div>
+</div>
+EVENT11
     )
   );
 ?>
