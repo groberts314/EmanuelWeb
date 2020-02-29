fb235d4f74390b34b813bf632f422a74ed784aa2
Geoffrey Roberts <groberts314@yahoo.com>
2020-02-29 09:28:00 -0800

    Tweaking Newsletter Page Layout and Styling
    This is a WIP, but I kind of made it live early.  It's mostly good; now we get the thumbnail above the fold on mobile and tablet viewport widths.  It has an issue at narrower mobile (below 500px) where month/year links will wrap for no apparent reason (because the width is artificially constrained).  Hopefully when I replace the widths with accordions, it'll be better.  Progress, not perfection.

--- newsletter.php
+++ newsletter.php
@@ -12,57 +12,62 @@
     <?php require_once('./partials/masthead-top-nav.phtml') ?>
 
     <div class="row main-content inner-page-content">
-      <div class="col-xs-12 col-sm-8">
+      <div class="col-xs-12 col-md-8">
         <h1 class="page-title">&ldquo;The Good News&rdquo; &ndash; Emanuel&rsquo;s Monthly Newsletter</h1>
-        <div class="row">
-          <div class="col-xs-12">
+        <div class="newsletter-container">
+          <div class="newsletter-issue-list">
             <h2 class="section-title no-top-margin">Current Issue</h2>
             <p>
               <a href="../newsletters/newsletter-2020-03.pdf" target="_blank">March 2020</a>
             </p>
             <h2 class="section-title">Past Issues</h2>
+            <div class="newsletter-additional-issues-list">
+              <div class="col-xs-12 col-md-4">
+                <ul class="list newsletter-list">
+                  <li><a href="../newsletters/newsletter-2020-02.pdf" target="_blank">February 2020</a></li>
+                  <li><a href="../newsletters/newsletter-2020-01.pdf" target="_blank">January 2020</a></li>
+                </ul>
+              </div>
+              <div class="col-xs-12 col-md-4">
+                <ul class="list newsletter-list">
+                  <li><a href="../newsletters/newsletter-2019-12.pdf" target="_blank">December 2019</a></li>
+                  <li><a href="../newsletters/newsletter-2019-11.pdf" target="_blank">November 2019</a></li>
+                  <li><a href="../newsletters/newsletter-2019-10.pdf" target="_blank">October 2019</a></li>
+                  <li><a href="../newsletters/newsletter-2019-09.pdf" target="_blank">September 2019</a></li>
+                  <li><a href="../newsletters/newsletter-2019-08.pdf" target="_blank">August 2019</a></li>
+                  <li><a href="../newsletters/newsletter-2019-07.pdf" target="_blank">July 2019</a></li>
+                  <li><a href="../newsletters/newsletter-2019-06.pdf" target="_blank">June 2019</a></li>
+                  <li><a href="../newsletters/newsletter-2019-05.pdf" target="_blank">May 2019</a></li>
+                  <li><a href="../newsletters/newsletter-2019-04.pdf" target="_blank">April 2019</a></li>
+                  <li><a href="../newsletters/newsletter-2019-03.pdf" target="_blank">March 2019</a></li>
+                  <li><a href="../newsletters/newsletter-2019-02.pdf" target="_blank">February 2019</a></li>
+                  <li><a href="../newsletters/newsletter-2019-01.pdf" target="_blank">January 2019</a></li>
+                </ul>
+              </div>
+              <div class="col-xs-12 col-md-4">
+                <ul class="list newsletter-list">
+                  <li><a href="../newsletters/newsletter-2018-12.pdf" target="_blank">December 2018</a></li>
+                  <li><a href="../newsletters/newsletter-2018-11.pdf" target="_blank">November 2018</a></li>
+                  <li><a href="../newsletters/newsletter-2018-10.pdf" target="_blank">October 2018</a></li>
+                  <li><a href="../newsletters/newsletter-2018-09.pdf" target="_blank">September 2018</a></li>
+                  <li><a href="../newsletters/newsletter-2018-08.pdf" target="_blank">August 2018</a></li>
+                  <li><a href="../newsletters/newsletter-2018-07.pdf" target="_blank">July 2018</a></li>
+                  <li><a href="../newsletters/newsletter-2018-06.pdf" target="_blank">June 2018</a></li>
+                  <li><a href="../newsletters/newsletter-2018-05.pdf" target="_blank">May 2018</a></li>
+                  <li><a href="../newsletters/newsletter-2018-04.pdf" target="_blank">April 2018</a></li>
+                  <li><a href="../newsletters/newsletter-2018-03.pdf" target="_blank">March 2018</a></li>
+                  <li><a href="../newsletters/newsletter-2018-02.pdf" target="_blank">February 2018</a></li>
+                  <li><a href="../newsletters/newsletter-2018-01.pdf" target="_blank">January 2018</a></li>
+                </ul>
+              </div>
+            </div>
           </div>
-          <div class="col-xs-12 col-sm-4">
-            <ul class="list newsletter-list">
-              <li><a href="../newsletters/newsletter-2020-02.pdf" target="_blank">February 2020</a></li>
-              <li><a href="../newsletters/newsletter-2020-01.pdf" target="_blank">January 2020</a></li>
-            </ul>
-          </div>
-          <div class="col-xs-12 col-sm-4">
-            <ul class="list newsletter-list">
-              <li><a href="../newsletters/newsletter-2019-12.pdf" target="_blank">December 2019</a></li>
-              <li><a href="../newsletters/newsletter-2019-11.pdf" target="_blank">November 2019</a></li>
-              <li><a href="../newsletters/newsletter-2019-10.pdf" target="_blank">October 2019</a></li>
-              <li><a href="../newsletters/newsletter-2019-09.pdf" target="_blank">September 2019</a></li>
-              <li><a href="../newsletters/newsletter-2019-08.pdf" target="_blank">August 2019</a></li>
-              <li><a href="../newsletters/newsletter-2019-07.pdf" target="_blank">July 2019</a></li>
-              <li><a href="../newsletters/newsletter-2019-06.pdf" target="_blank">June 2019</a></li>
-              <li><a href="../newsletters/newsletter-2019-05.pdf" target="_blank">May 2019</a></li>
-              <li><a href="../newsletters/newsletter-2019-04.pdf" target="_blank">April 2019</a></li>
-              <li><a href="../newsletters/newsletter-2019-03.pdf" target="_blank">March 2019</a></li>
-              <li><a href="../newsletters/newsletter-2019-02.pdf" target="_blank">February 2019</a></li>
-              <li><a href="../newsletters/newsletter-2019-01.pdf" target="_blank">January 2019</a></li>
-            </ul>
-          </div>
-          <div class="col-xs-12 col-sm-4">
-            <ul class="list newsletter-list">
-              <li><a href="../newsletters/newsletter-2018-12.pdf" target="_blank">December 2018</a></li>
-              <li><a href="../newsletters/newsletter-2018-11.pdf" target="_blank">November 2018</a></li>
-              <li><a href="../newsletters/newsletter-2018-10.pdf" target="_blank">October 2018</a></li>
-              <li><a href="../newsletters/newsletter-2018-09.pdf" target="_blank">September 2018</a></li>
-              <li><a href="../newsletters/newsletter-2018-08.pdf" target="_blank">August 2018</a></li>
-              <li><a href="../newsletters/newsletter-2018-07.pdf" target="_blank">July 2018</a></li>
-              <li><a href="../newsletters/newsletter-2018-06.pdf" target="_blank">June 2018</a></li>
-              <li><a href="../newsletters/newsletter-2018-05.pdf" target="_blank">May 2018</a></li>
-              <li><a href="../newsletters/newsletter-2018-04.pdf" target="_blank">April 2018</a></li>
-              <li><a href="../newsletters/newsletter-2018-03.pdf" target="_blank">March 2018</a></li>
-              <li><a href="../newsletters/newsletter-2018-02.pdf" target="_blank">February 2018</a></li>
-              <li><a href="../newsletters/newsletter-2018-01.pdf" target="_blank">January 2018</a></li>
-            </ul>
+          <div class="newsletter-current-issue-thumbnail-mobile hidden-md hidden-lg">
+            <img src="images/newsletter/newsletter-2020-03.jpg" alt="The Good News: March 2020" />
           </div>
         </div>
       </div>
-      <div class="col-xs-12 col-sm-4 photo-right no-btm-margin">
+      <div class="col-xs-12 col-md-4 photo-right no-btm-margin hidden-xs hidden-sm">
         <img src="images/newsletter/newsletter-2020-03.jpg" alt="The Good News: March 2020" />
       </div>
     </div>
