4c395a65a5b14bef7af0eba75f44e3e252038f14
Geoffrey Roberts <groberts314@yahoo.com>
2020-03-01 05:27:31 -0800

    Updated Newsletter Page with Accordions for Past Years

--- newsletter.php
+++ newsletter.php
@@ -20,47 +20,7 @@
             <p>
               <a href="../newsletters/newsletter-2020-03.pdf" target="_blank">March 2020</a>
             </p>
-            <h2 class="section-title">Past Issues</h2>
-            <div class="newsletter-additional-issues-list">
-              <div class="col-xs-12 col-md-4">
-                <ul class="list newsletter-list">
-                  <li><a href="../newsletters/newsletter-2020-02.pdf" target="_blank">February 2020</a></li>
-                  <li><a href="../newsletters/newsletter-2020-01.pdf" target="_blank">January 2020</a></li>
-                </ul>
-              </div>
-              <div class="col-xs-12 col-md-4">
-                <ul class="list newsletter-list">
-                  <li><a href="../newsletters/newsletter-2019-12.pdf" target="_blank">December 2019</a></li>
-                  <li><a href="../newsletters/newsletter-2019-11.pdf" target="_blank">November 2019</a></li>
-                  <li><a href="../newsletters/newsletter-2019-10.pdf" target="_blank">October 2019</a></li>
-                  <li><a href="../newsletters/newsletter-2019-09.pdf" target="_blank">September 2019</a></li>
-                  <li><a href="../newsletters/newsletter-2019-08.pdf" target="_blank">August 2019</a></li>
-                  <li><a href="../newsletters/newsletter-2019-07.pdf" target="_blank">July 2019</a></li>
-                  <li><a href="../newsletters/newsletter-2019-06.pdf" target="_blank">June 2019</a></li>
-                  <li><a href="../newsletters/newsletter-2019-05.pdf" target="_blank">May 2019</a></li>
-                  <li><a href="../newsletters/newsletter-2019-04.pdf" target="_blank">April 2019</a></li>
-                  <li><a href="../newsletters/newsletter-2019-03.pdf" target="_blank">March 2019</a></li>
-                  <li><a href="../newsletters/newsletter-2019-02.pdf" target="_blank">February 2019</a></li>
-                  <li><a href="../newsletters/newsletter-2019-01.pdf" target="_blank">January 2019</a></li>
-                </ul>
-              </div>
-              <div class="col-xs-12 col-md-4">
-                <ul class="list newsletter-list">
-                  <li><a href="../newsletters/newsletter-2018-12.pdf" target="_blank">December 2018</a></li>
-                  <li><a href="../newsletters/newsletter-2018-11.pdf" target="_blank">November 2018</a></li>
-                  <li><a href="../newsletters/newsletter-2018-10.pdf" target="_blank">October 2018</a></li>
-                  <li><a href="../newsletters/newsletter-2018-09.pdf" target="_blank">September 2018</a></li>
-                  <li><a href="../newsletters/newsletter-2018-08.pdf" target="_blank">August 2018</a></li>
-                  <li><a href="../newsletters/newsletter-2018-07.pdf" target="_blank">July 2018</a></li>
-                  <li><a href="../newsletters/newsletter-2018-06.pdf" target="_blank">June 2018</a></li>
-                  <li><a href="../newsletters/newsletter-2018-05.pdf" target="_blank">May 2018</a></li>
-                  <li><a href="../newsletters/newsletter-2018-04.pdf" target="_blank">April 2018</a></li>
-                  <li><a href="../newsletters/newsletter-2018-03.pdf" target="_blank">March 2018</a></li>
-                  <li><a href="../newsletters/newsletter-2018-02.pdf" target="_blank">February 2018</a></li>
-                  <li><a href="../newsletters/newsletter-2018-01.pdf" target="_blank">January 2018</a></li>
-                </ul>
-              </div>
-            </div>
+            <h2 class="section-title past-issues">Past Issues</h2>
           </div>
           <div class="newsletter-current-issue-thumbnail-mobile hidden-md hidden-lg">
             <img src="images/newsletter/newsletter-2020-03.jpg" alt="The Good News: March 2020" />
@@ -69,6 +29,98 @@
       </div>
       <div class="col-xs-12 col-md-4 photo-right no-btm-margin hidden-xs hidden-sm">
         <img src="images/newsletter/newsletter-2020-03.jpg" alt="The Good News: March 2020" />
+      </div>
+      <div class="col-xs-12">
+        <div class="newsletter-additional-issues-list">
+          <div class="panel-group" id="newsletter-archive-accordion" role="tablist" aria-multiselectable="true">
+            <div class="panel panel-default">
+              <div class="panel-heading" role="tab" id="heading-2020">
+                <h4 class="panel-title">
+                <a role="button" data-toggle="collapse" data-parent="#newsletter-archive-accordion" href="#collapse-2020" aria-expanded="true" aria-controls="collapse-2020">
+                  2020
+                </a>
+                </h4>
+              </div>
+              <div id="collapse-2020" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="heading-2020">
+                <div class="panel-body">
+                  <div class="newsletter-year-issue-list">
+                    <ul class="list-group">
+                      <li class="list-group-item"><a href="../newsletters/newsletter-2020-02.pdf" target="_blank">February 2020</a></li>
+                      <li class="list-group-item"><a href="../newsletters/newsletter-2020-01.pdf" target="_blank">January 2020</a></li>
+                    </ul>
+                  </div>
+                  <div class="newsletter-year-issue-thumbnail">
+                    <img src="images/newsletter/newsletter-2020-02.jpg" alt="The Good News: February 2020" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="panel panel-default">
+              <div class="panel-heading" role="tab" id="heading-2019">
+                <h4 class="panel-title">
+                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#newsletter-archive-accordion" href="#collapse-2019" aria-expanded="false" aria-controls="collapse-2019">
+                  2019
+                </a>
+                </h4>
+              </div>
+              <div id="collapse-2019" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading-2019">
+                <div class="panel-body">
+                  <div class="newsletter-year-issue-list">
+                    <ul class="list-group">
+                      <li class="list-group-item"><a href="../newsletters/newsletter-2019-12.pdf" target="_blank">December 2019</a></li>
+                      <li class="list-group-item"><a href="../newsletters/newsletter-2019-11.pdf" target="_blank">November 2019</a></li>
+                      <li class="list-group-item"><a href="../newsletters/newsletter-2019-10.pdf" target="_blank">October 2019</a></li>
+                      <li class="list-group-item"><a href="../newsletters/newsletter-2019-09.pdf" target="_blank">September 2019</a></li>
+                      <li class="list-group-item"><a href="../newsletters/newsletter-2019-08.pdf" target="_blank">August 2019</a></li>
+                      <li class="list-group-item"><a href="../newsletters/newsletter-2019-07.pdf" target="_blank">July 2019</a></li>
+                      <li class="list-group-item"><a href="../newsletters/newsletter-2019-06.pdf" target="_blank">June 2019</a></li>
+                      <li class="list-group-item"><a href="../newsletters/newsletter-2019-05.pdf" target="_blank">May 2019</a></li>
+                      <li class="list-group-item"><a href="../newsletters/newsletter-2019-04.pdf" target="_blank">April 2019</a></li>
+                      <li class="list-group-item"><a href="../newsletters/newsletter-2019-03.pdf" target="_blank">March 2019</a></li>
+                      <li class="list-group-item"><a href="../newsletters/newsletter-2019-02.pdf" target="_blank">February 2019</a></li>
+                      <li class="list-group-item"><a href="../newsletters/newsletter-2019-01.pdf" target="_blank">January 2019</a></li>
+                    </ul>
+                  </div>
+                  <div class="newsletter-year-issue-thumbnail">
+                    <img src="images/newsletter/newsletter-2019-12.jpg" alt="The Good News: December 2019" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="panel panel-default">
+              <div class="panel-heading" role="tab" id="heading-2018">
+                <h4 class="panel-title">
+                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#newsletter-archive-accordion" href="#collapse-2018" aria-expanded="false" aria-controls="collapse-2018">
+                  2018
+                </a>
+                </h4>
+              </div>
+              <div id="collapse-2018" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading-2018">
+                <div class="panel-body">
+                  <div class="newsletter-year-issue-list">
+                    <ul class="list-group">
+                      <li class="list-group-item"><a href="../newsletters/newsletter-2018-12.pdf" target="_blank">December 2018</a></li>
+                      <li class="list-group-item"><a href="../newsletters/newsletter-2018-11.pdf" target="_blank">November 2018</a></li>
+                      <li class="list-group-item"><a href="../newsletters/newsletter-2018-10.pdf" target="_blank">October 2018</a></li>
+                      <li class="list-group-item"><a href="../newsletters/newsletter-2018-09.pdf" target="_blank">September 2018</a></li>
+                      <li class="list-group-item"><a href="../newsletters/newsletter-2018-08.pdf" target="_blank">August 2018</a></li>
+                      <li class="list-group-item"><a href="../newsletters/newsletter-2018-07.pdf" target="_blank">July 2018</a></li>
+                      <li class="list-group-item"><a href="../newsletters/newsletter-2018-06.pdf" target="_blank">June 2018</a></li>
+                      <li class="list-group-item"><a href="../newsletters/newsletter-2018-05.pdf" target="_blank">May 2018</a></li>
+                      <li class="list-group-item"><a href="../newsletters/newsletter-2018-04.pdf" target="_blank">April 2018</a></li>
+                      <li class="list-group-item"><a href="../newsletters/newsletter-2018-03.pdf" target="_blank">March 2018</a></li>
+                      <li class="list-group-item"><a href="../newsletters/newsletter-2018-02.pdf" target="_blank">February 2018</a></li>
+                      <li class="list-group-item"><a href="../newsletters/newsletter-2018-01.pdf" target="_blank">January 2018</a></li>
+                    </ul>
+                  </div>
+                  <div class="newsletter-year-issue-thumbnail">
+                    <img src="images/newsletter/newsletter-2018-12.jpg" alt="The Good News: December 2018" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
 
