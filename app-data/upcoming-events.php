41ca152db9d3780dbfb649810ce24263dcc7eadd
Geoffrey Roberts <groberts314@yahoo.com>
2020-04-26 06:06:19 -0700

    Add Sunday School Online page w/ video & link to it from Upcoming Events

--- app-data/upcoming-events.php
+++ app-data/upcoming-events.php
@@ -131,10 +131,16 @@ HTML
     ),
     11 => array(
       'date' => '2020-04-26',
+      'keepUntil' => '2020-05-03',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
     Second Sunday of Easter Morning Online Worship
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <a href="/sunday-school-online">Sunday School Online</a>
   </div>
 </div>
 HTML
---
+++ sunday-school-online.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php require_once('./partials/head-metas.phtml') ?>
+  <title>Sunday School Online | Emanuel Evangelical Lutheran Church</title>
+  <?php require_once('./partials/head-includes.phtml') ?>
+</head>
+
+<body>
+  <div class="container layout-v2">
+    <?php require_once('./partials/masthead-top-nav.phtml') ?>
+
+    <div class="row main-content inner-page-content">
+      <div class="col-xs-12 col-sm-8">
+        <h1 class="page-title">Sunday School Online</h1>
+        <div class="fluid-width-video-wrapper">
+          <iframe class="video-iframe" src="https://www.youtube-nocookie.com/embed/6cTscKhan1E" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-4 photo-right no-btm-margin">
+        <img src="images/who-we-are/emanuel-sign.jpg" alt="Emanuel: God With Us" />
+      </div>
+    </div>
+
+    <?php require_once('./partials/footer.phtml') ?>
+  </div>
+
+  <?php require_once('./partials/footer-includes.phtml') ?>
+</body>
+
+</html>
