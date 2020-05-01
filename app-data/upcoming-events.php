f2d9ace23d0c172cf3212e77fd98367247d65182
Geoffrey Roberts <groberts314@yahoo.com>
2020-05-01 15:59:53 -0700

    Update Upcoming Events to Add Sunday May 3

--- app-data/upcoming-events.php
+++ app-data/upcoming-events.php
@@ -1,135 +1,6 @@
 <?php
   return array(
     0 => array(
-      'date' => '2020-03-29',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    Sunday of Lent Morning Online Worship
-  </div>
-</div>
-HTML
-    ),
-    1 => array(
-      'date' => '2020-04-01',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    Zoom Bible Study
-  </div>
-  <div class="event-time">
-    9:00 AM
-  </div>
-</div>
-HTML
-    ),
-    2 => array(
-      'date' => '2020-04-02',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    Lenten Midweek virtual gathering on
-    <a href="https://www.facebook.com/emanuel.lutheran.1">Facebook</a>
-  </div>
-  <div class="event-time">
-    6:30 PM
-  </div>
-</div>
-HTML
-    ),
-    3 => array(
-      'date' => '2020-04-05',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    Palm Sunday Morning Online worship
-  </div>
-</div>
-HTML
-    ),
-    4 => array(
-      'date' => '2020-04-08',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    Zoom Bible Study
-  </div>
-  <div class="event-time">
-    9:00 AM
-  </div>
-</div>
-HTML
-    ),
-    5 => array(
-      'date' => '2020-04-09',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    Maundy Thursday Evening Online worship
-  </div>
-</div>
-HTML
-    ),
-    6 => array(
-      'date' => '2020-04-10',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    Good Friday Evening Online worship
-  </div>
-</div>
-HTML
-    ),
-    7 => array(
-      'date' => '2020-04-12',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Easter Sunday Celebration</strong>
-    <br  />
-    Online Worship Service<br />
-    Zoom Virtual Easter Breakfast
-  </div>
-</div>
-HTML
-    ),
-    8 => array(
-      'date' => '2020-04-15',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    Zoom Bible Study
-  </div>
-  <div class="event-time">
-    9:00 AM
-  </div>
-</div>
-HTML
-    ),
-    9 => array(
-      'date' => '2020-04-19',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    First Sunday of Easter Morning Online Worship
-  </div>
-</div>
-HTML
-    ),
-    10 => array(
-      'date' => '2020-04-22',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    Zoom Bible Study
-  </div>
-  <div class="event-time">
-    9:00 AM
-  </div>
-</div>
-HTML
-    ),
-    11 => array(
       'date' => '2020-04-26',
       'keepUntil' => '2020-05-03',
       'content' => <<<HTML
@@ -141,6 +12,16 @@ HTML
 <div class="event-detail">
   <div class="event-description">
     <a href="/sunday-school-online">Sunday School Online</a>
+  </div>
+</div>
+HTML
+    ),
+    1 => array(
+      'date' => '2020-05-03',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    Third Sunday of Easter Morning Online Worship
   </div>
 </div>
 HTML
