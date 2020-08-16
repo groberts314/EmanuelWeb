e814be4d4936bef4ffde905a26809eeea11a5ddd
Geoffrey Roberts <groberts314@yahoo.com>
2020-08-16 07:40:14 -0700

    Additional Upcoming Events for August an September 2020

--- app-data/upcoming-events.php
+++ app-data/upcoming-events.php
@@ -2,18 +2,201 @@
   return array(
     0 => array(
       'date' => '2020-08-16',
-      'keepUntil' => '2020-08-23',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    Sunday Morning Online Worship
+    <strong>Sunday Morning Online Worship</strong>
   </div>
 </div>
 <div class="event-detail">
   <div class="event-description">
-    <a href="/sunday-school-online">Sunday School Online</a>
+    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
   </div>
 </div>
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Hymns &amp; Praise Songs Sing Along</strong>
+    <br />
+    (email <a href="mailto:churchinfo@elclh.org">churchinfo@elclh.org</a> for the Zoom Link)
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">&nbsp;</div>
+  <div class="event-time">
+    10:30 AM
+  </div>
+</div>
+HTML
+    ),
+    1 => array(
+      'date' => '2020-08-23',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Sunday Morning Online Worship</strong>
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Drive Thru Worship</strong>
+    <br />
+    RSVP required. Information <a href="https://www.youtube.com/watch?v=jHWKjSmaFls" target="_blank">here</a>.
+  </div>
+  <div class="event-time">
+    8 AM
+  </div>
+</div>
+HTML
+    ),
+    2 => array(
+      'date' => '2020-08-30',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Sunday Morning Online Worship</strong>
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Hymns &amp; Praise Songs Sing Along</strong>
+    <br />
+    (email <a href="mailto:churchinfo@elclh.org">churchinfo@elclh.org</a> for the Zoom Link)
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">&nbsp;</div>
+  <div class="event-time">
+    10:30 AM
+  </div>
+</div>
+HTML
+    ),
+    3 => array(
+      'date' => '2020-09-06',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Sunday Morning Online Worship</strong>
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Drive Thru Worship</strong>
+    <br />
+    RSVP required. Information <a href="https://www.youtube.com/watch?v=jHWKjSmaFls" target="_blank">here</a>.
+  </div>
+  <div class="event-time">
+    8 AM
+  </div>
+</div>
+HTML
+    ),
+    4 => array(
+      'date' => '2020-09-13',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Sunday Morning Online Worship</strong>
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Hymns &amp; Praise Songs Sing Along</strong>
+    <br />
+    (email <a href="mailto:churchinfo@elclh.org">churchinfo@elclh.org</a> for the Zoom Link)
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">&nbsp;</div>
+  <div class="event-time">
+    10:30 AM
+  </div>
+</div>
+HTML
+    ),
+    5 => array(
+      'date' => '2020-09-20',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Sunday Morning Online Worship</strong>
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Drive Thru Worship</strong>
+    <br />
+    RSVP required. Information <a href="https://www.youtube.com/watch?v=jHWKjSmaFls" target="_blank">here</a>.
+  </div>
+  <div class="event-time">
+    8 AM
+  </div>
+</div>
+HTML
+    ),
+    6 => array(
+      'date' => '2020-09-13',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Sunday Morning Online Worship</strong>
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Hymns &amp; Praise Songs Sing Along</strong>
+    <br />
+    (email <a href="mailto:churchinfo@elclh.org">churchinfo@elclh.org</a> for the Zoom Link)
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">&nbsp;</div>
+  <div class="event-time">
+    10:30 AM
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Grief Service</strong>
+    <br />
+    Upper Parking Lot. More information coming soon.
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">&nbsp;</div>
+  <div class="event-time">
+    6 PM
+  </div>
 HTML
     )
   );
