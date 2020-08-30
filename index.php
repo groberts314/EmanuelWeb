5509ea692b93e5fd3bbb2cfc623ad5ccb982bef8
Geoffrey Roberts <groberts314@yahoo.com>
2020-08-30 06:23:41 -0700

    Updates for August 30, 2020

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
           <?php /*require_once('./partials/homepage-v2-photo-carousel.phtml')*/ ?>
           <?php /* Worship Video embedded from YouTube in "fluid width" container */ ?>
           <div class="fluid-width-video-wrapper">
-            <iframe class="video-iframe" src="https://www.youtube-nocookie.com/embed/fjwIZYYDuZE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe class="video-iframe" src="https://www.youtube-nocookie.com/embed/rSk8KOwqYeA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           </div>
           <p style="margin-top: 22px; text-align: center">
-            <strong>Emanuel Online Sunday August 23, 2020</strong><br />
+            <strong>Emanuel Online Sunday August 30, 2020</strong><br />
           </p>
       </div>
       <div class="module-container upcoming-events">
