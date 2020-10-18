b15d732b5a1f2dea79e12845d81d16e1deb245b7
Geoffrey Roberts <groberts314@yahoo.com>
2020-10-18 04:50:05 -0700

    Updates for October 18, 2020
    * Homepage video
    * Sunday School Online
    * Update homepage COVID 19 restriction notices (and accompanying CSS to fix height of Upcoming Events module to match that of left-hand side module with Welcome and Worship Service Schedule)

--- sunday-school-online.php
+++ sunday-school-online.php
@@ -15,6 +15,11 @@
       <div class="col-xs-12 col-sm-8">
         <h1 class="page-title">Sunday School Online</h1>
         <div class="fluid-width-video-wrapper">
+          <iframe class="video-iframe" src="https://www.youtube-nocookie.com/embed/Po2WV7bG53g" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+
+        <h2 class="section-title">Lesson #23: Sunday, October 11, 2020</h2>
+        <div class="fluid-width-video-wrapper">
           <iframe class="video-iframe" src="https://www.youtube-nocookie.com/embed/OxeqZGEPj-A" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
 
