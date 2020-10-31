35ea89af647e87b0177c9d4cc40c6dad9e561df7
Geoffrey Roberts <groberts314@yahoo.com>
2020-10-31 03:52:10 -0700

    Updates for month of November 2020
    * November 2020 Newsletter
    * November 2020 Calendar
    * Upcoming Events

--- app-data/upcoming-events.php
+++ app-data/upcoming-events.php
@@ -1,216 +1,6 @@
 <?php
   return array(
     0 => array(
-      'date' => '2020-09-27',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Sunday Morning Online Worship</strong>
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Hymns &amp; Praise Songs Sing Along</strong>
-    <br />
-    (email <a href="mailto:churchinfo@elclh.org">churchinfo@elclh.org</a> for the Zoom Link)
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">&nbsp;</div>
-  <div class="event-time">
-    10:30 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Grief Service</strong>
-    <br />
-    Upper Parking Lot. More information coming soon.
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">&nbsp;</div>
-  <div class="event-time">
-    6 PM
-  </div>
-HTML
-    ),
-    1 => array(
-      'date' => '2020-10-01',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <a href="/event-fliers/2020-10-01-parent-support-group.jpg" target="_blank"><strong>Virtual Parent Support Group</strong></a>
-    <br />
-    Contact Church Office for Zoom link.
-  </div>
-  <div class="event-time">
-    7 PM
-  </div>
-</div>
-HTML
-    ),
-    2 => array(
-      'date' => '2020-10-04',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Sunday Morning Online Worship</strong>
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <a href="/event-fliers/2020-10-drive-thru-communion.jpg" target="_blank"><strong>Drive Thru Communion and Prayer Service</strong></a>
-    <br />
-    RSVP required. Information <a href="https://www.youtube.com/watch?v=jHWKjSmaFls" target="_blank">here</a>.
-  </div>
-  <div class="event-time">
-    8 AM
-  </div>
-</div>
-HTML
-    ),
-    3 => array(
-      'date' => '2020-10-11',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Sunday Morning Online Worship</strong>
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <a href="/event-fliers/2020-10-sing-along.jpg" target="_blank"><strong>Virtual Hymn and Praise Song Sing-Along</strong></a>
-    <br />
-    Zoom link found in Weekly Bulletin.<br />
-    Email song requests to <a href="mailto:churchinfo@elclh.org">churchinfo@elclh.org</a> by Wed Oct 7.
-  </div>
-  <div class="event-time">
-    10:30 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <a href="/event-fliers/2020-10-in-person-outdoor-worship.jpg" target="_blank"><strong>In-Person Outdoor Worship</strong></a>
-    <br />
-    RSVP to church office by noon on Friday, October 9.
-  </div>
-  <div class="event-time">
-    5 PM
-  </div>
-</div>
-HTML
-    ),
-    4 => array(
-      'date' => '2020-10-17',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <a href="/event-fliers/2020-10-17-shred-event.jpg"><strong>Thrivent - Free Shred Event &amp; Food Drive</strong></a>
-    <br />
-    at Christ Lutheran Church in Brea.
-  </div>
-  <div class="event-time">
-    8 AM
-  </div>
-</div>
-HTML
-    ),
-    5 => array(
-      'date' => '2020-10-18',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Sunday Morning Online Worship</strong>
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <a href="/event-fliers/2020-10-drive-thru-communion.jpg" target="_blank"><strong>Drive Thru Communion and Prayer Service</strong></a>
-    <br />
-    RSVP required. Information <a href="https://www.youtube.com/watch?v=jHWKjSmaFls" target="_blank">here</a>.
-  </div>
-  <div class="event-time">
-    8 AM
-  </div>
-</div>
-HTML
-    ),
-    6 => array(
-      'date' => '2020-10-20',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <a href="/event-fliers/2020-10-20-joy-circle.jpg"><strong>Joy Circle</strong></a>
-    <br />
-    Contact Pastor Cate at <a href="mailto:pastorcate@elclh.org">pastorcate@elclh.org</a> for the Zoom link.
-  </div>
-  <div class="event-time">
-    9:30 AM
-  </div>
-</div>
-HTML
-    ),
-    7 => array(
-      'date' => '2020-10-25',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Sunday Morning Online Worship</strong>
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <a href="/event-fliers/2020-10-sing-along.jpg" target="_blank"><strong>Virtual Hymn and Praise Song Sing-Along</strong></a>
-    <br />
-    Zoom link found in Weekly Bulletin.<br />
-    Email song requests to <a href="mailto:churchinfo@elclh.org">churchinfo@elclh.org</a> by Wed Oct 21.
-  </div>
-  <div class="event-time">
-    10:30 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <a href="/event-fliers/2020-10-in-person-outdoor-worship.jpg" target="_blank"><strong>In-Person Outdoor Worship</strong></a>
-    <br />
-    RSVP to church office by noon on Friday, October 23.
-    <br /><br />
-    This Reformation Sunday worship service will feature the
-    <a href="2020-10-25-confirmation.jpg" target="_blank"><strong>Confirmands&rsquo; Affirmation of Baptism</strong></a>.
-  </div>
-  <div class="event-time">
-    5 PM
-  </div>
-</div>
-HTML
-    ),
-    8 => array(
       'date' => '2020-11-01',
       'content' => <<<HTML
 <div class="event-detail">
@@ -225,9 +15,147 @@ HTML
 </div>
 <div class="event-detail">
   <div class="event-description">
-    <a href="/event-fliers/2020-10-drive-thru-communion.jpg" target="_blank"><strong>Drive Thru Communion and Prayer Service</strong></a>
+    <a href="/event-fliers/2020-11-drive-thru-worship.jpg" target="_blank"><strong>Drive Thru Communion and Prayer Service</strong></a>
     <br />
     RSVP required. Information <a href="https://www.youtube.com/watch?v=jHWKjSmaFls" target="_blank">here</a>.
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+HTML
+    ),
+    1 => array(
+      'date' => '2020-11-05',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <a href="/event-fliers/2020-11-05-parent-support.jpg" target="_blank"><strong>Virtual Parent Support Group</strong></a>
+    <br />
+    (email <a href="mailto:churchinfo@elclh.org">churchinfo@elclh.org</a> for the Zoom Link)
+  </div>
+  <div class="event-time">
+    7:30 PM
+  </div>
+</div>
+HTML
+    ),
+    2 => array(
+      'date' => '2020-11-08',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Sunday Morning Online Worship</strong>
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <a href="/event-fliers/2020-11-parking-lot-worship.jpg" target="_blank"><strong>Parking Lot Worship Service</strong></a>
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+HTML
+    ),
+    3 => array(
+      'date' => '2020-11-15',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Sunday Morning Online Worship</strong>
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <a href="/event-fliers/2020-11-drive-thru-worship.jpg" target="_blank"><strong>Drive Thru Communion and Prayer Service</strong></a>
+    <br />
+    RSVP required. Information <a href="https://www.youtube.com/watch?v=jHWKjSmaFls" target="_blank">here</a>.
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <a href="/event-fliers/2020-11-15-annual-meeting.jpg" target="_blank"><strong>Annual Congregational Meeting</strong></a>
+    <br />
+    In-person in upper parking lot and virtual via Zoom.<br />
+    RSVP to church office by noon Friday, November 13.
+  </div>
+  <div class="event-time">
+    5 PM
+  </div>
+</div>
+HTML
+    ),
+    4 => array(
+      'date' => '2020-11-17',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <a href="/event-fliers/2020-11-17-joy-circle.jpg" target="_blank"><strong>Joy Circle on Zoom</strong></a>
+    <br />
+    (email Pastor Cate at <a href="mailto:pastorcate@elclh.org">pastorcate@elclh.org</a> for the Zoom Link)
+  </div>
+  <div class="event-time">
+    9:30 AM
+  </div>
+</div>
+HTML
+    ),
+    5 => array(
+      'date' => '2020-11-22',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Sunday Morning Online Worship</strong>
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <a href="/event-fliers/2020-11-parking-lot-worship.jpg" target="_blank"><strong>Parking Lot Worship Service</strong></a>
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+HTML
+    ),
+    6 => array(
+      'date' => '2020-11-29',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Sunday Morning Online Worship</strong><br />
+    First Sunday of Advent
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <a href="/event-fliers/2020-11-29-parking-lot-with-communion.jpg" target="_blank">
+      <strong>Parking Lot Worship Service with Communion</strong>
+    </a>
   </div>
   <div class="event-time">
     10 AM
