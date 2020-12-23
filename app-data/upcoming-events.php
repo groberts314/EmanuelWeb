e825c296d038d23a1f2822da3910669c1dfcdd84
Geoffrey Roberts <groberts314@yahoo.com>
2020-12-23 15:55:48 -0800

    Updates for January 2021
    * Newsletter
    * Calendar
    * Upcoming Events

--- app-data/upcoming-events.php
+++ app-data/upcoming-events.php
@@ -1,51 +1,16 @@
 <?php
   return array(
     0 => array(
-      'date' => '2020-12-13',
+      'date' => '2020-12-24',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    <strong>Sunday Morning Online Worship</strong>
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
+    <strong>Christmas Eve Online Worship</strong>
   </div>
 </div>
 HTML
     ),
     1 => array(
-      'date' => '2020-12-15',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <a href="/event-fliers/2020-12-15-joy-circle.jpg" target="_blank">
-      <strong>Joy Circle on Zoom</strong>
-    </a>
-  </div>
-  <div class="event-time">
-    9:30 AM
-  </div>
-</div>
-HTML
-    ),
-    2 => array(
-      'date' => '2020-12-20',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Sunday Morning Online Worship</strong>
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
-  </div>
-</div>
-HTML
-    ),
-    3 => array(
       'date' => '2020-12-27',
       'content' => <<<HTML
 <div class="event-detail">
@@ -60,7 +25,7 @@ HTML
 </div>
 HTML
     ),
-    4 => array(
+    2 => array(
       'date' => '2021-01-03',
       'content' => <<<HTML
 <div class="event-detail">
@@ -75,7 +40,7 @@ HTML
 </div>
 HTML
     ),
-    5 => array(
+    3 => array(
       'date' => '2021-01-04',
       'content' => <<<HTML
 <div class="event-detail">
@@ -86,6 +51,81 @@ HTML
   </div>
   <div class="event-time">
     12 PM
+  </div>
+</div>
+HTML
+    ),
+    4 => array(
+      'date' => '2021-01-10',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Sunday Morning Online Worship</strong>
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
+  </div>
+</div>
+HTML
+    ),
+    5 => array(
+      'date' => '2021-01-17',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Sunday Morning Online Worship</strong>
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
+  </div>
+</div>
+HTML
+    ),
+    6 => array(
+      'date' => '2021-01-19',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <a href="/event-fliers/2021-01-19-joy-circle.jpg" target="_blank">
+      <strong>Joy Circle</strong>
+    </a>
+  </div>
+  <div class="event-time">
+    9:30 AM
+  </div>
+</div>
+HTML
+    ),
+    7 => array(
+      'date' => '2021-01-24',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Sunday Morning Online Worship</strong>
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
+  </div>
+</div>
+HTML
+    ),
+    8 => array(
+      'date' => '2021-01-31',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Sunday Morning Online Worship</strong>
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
   </div>
 </div>
 HTML
