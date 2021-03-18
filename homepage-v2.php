f2600063af8f1e0000aff6e21673443e31b9b4e1
Geoffrey Roberts <groberts314@yahoo.com>
2021-03-17 18:41:41 -0700

    Update homepage v2 Page for testing Vimeo Live Stream

--- homepage-v2.php
+++ homepage-v2.php
@@ -26,12 +26,24 @@
       <div class="module-container location">
           <?php require_once('./partials/homepage-v2-location-and-contact.phtml') ?>
       </div>
-      <div class="module-container photo-carousel">
-          <?php require_once('./partials/homepage-v2-photo-carousel.phtml') ?>
+      <div class="module-container photo-carousel" id="photo-carousel-container">
+          <?php /* Photo Carousel */ ?>
+          <?php /*require_once('./partials/homepage-v2-photo-carousel.phtml')*/ ?>
+          <?php /* Worship Video embedded from YouTube in "fluid width" container */ ?>
+          <div class="fluid-width-video-wrapper">
+            <?php /* YouTube */ ?>
+            <iframe class="video-iframe" src="https://www.youtube-nocookie.com/embed/wdoDCUctrxY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+            <?php /* Vimeo Live Stream */ ?>
+            <?php /*<iframe class="video-iframe" src="https://player.vimeo.com/video/525286406" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>*/ ?>
+          </div>
+          <p style="font-weight: bold; margin-top: 22px; text-align: center">
+            Emanuel Online Lenten Midweek Service Wednesday March 17, 2021
+          </p>
       </div>
       <div class="module-container upcoming-events">
-        <?php /*require_once('./partials/homepage-v2-upcoming-events.phtml')*/ ?>
-        <?php require_once('./partials/homepage-v2-temp-coronavirus-upcoming-events.phtml') ?>
+          <?php require_once('./partials/homepage-v2-upcoming-events.phtml') ?>
+          <?php /*require_once('./partials/homepage-v2-temp-coronavirus-upcoming-events.phtml')*/ ?>
       </div>
     </div>
 
