c4795b9fda211ec08c159b51f896fa3a32d0034d
Geoffrey Roberts <groberts314@yahoo.com>
2021-05-30 08:09:51 -0700

    Updates for June 2021
    * Newsletter
    * Calendar
    * Upcoming Events

--- app-data/upcoming-events.php
+++ app-data/upcoming-events.php
@@ -1,105 +1,6 @@
 <?php
   return array(
     0 => array(
-      'date' => '2021-05-02',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Sunday Worship</strong><br />
-    In-Person/Outdoor &amp; Live-Streamed Online
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
-  </div>
-</div>
-HTML
-    ),
-    1 => array(
-      'date' => '2021-05-09',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Sunday Worship</strong><br />
-    In-Person/Outdoor &amp; Live-Streamed Online
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
-  </div>
-</div>
-HTML
-    ),
-    2 => array(
-      'date' => '2021-05-16',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Sunday Worship</strong><br />
-    In-Person/Outdoor &amp; Live-Streamed Online
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <a href="event-fliers/2021-05-16-vicar-matt-farewell.jpg" target="_blank">
-      <strong>Vicar Matt&rsquo;s Farewell</strong>
-    </a>
-  </div>
-  <div class="event-time">5 PM</div>
-</div>
-HTML
-    ),
-    3 => array(
-      'date' => '2021-05-18',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <a href="event-fliers/2021-05-18-joy-circle.jpg" target="_blank">
-      <strong>Joy Circle</strong>
-    </a>
-    <br />
-    Meeting <strong><em>IN PERSON</em></strong> and on Zoom.
-  </div>
-  <div class="event-time">9:30 AM</div>
-</div>
-HTML
-    ),
-    4 => array(
-      'date' => '2021-05-23',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Sunday Worship</strong><br />
-    In-Person/Indoor &amp; Live-Streamed Online
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
-  </div>
-</div>
-HTML
-    ),
-    5 => array(
       'date' => '2021-05-30',
       'content' => <<<HTML
 <div class="event-detail">
@@ -126,7 +27,7 @@ HTML
 </div>
 HTML
     ),
-    6 => array(
+    1 => array(
       'date' => '2021-06-05',
       'content' => <<<HTML
 <div class="event-detail">
@@ -140,7 +41,7 @@ HTML
 </div>
 HTML
     ),
-    7 => array(
+    2 => array(
       'date' => '2021-06-06',
       'content' => <<<HTML
 <div class="event-detail">
@@ -158,6 +59,63 @@ HTML
   </div>
 </div>
 HTML
-    )
-  );
+    ),
+    3 => array(
+      'date' => '2021-06-13',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Sunday Worship</strong><br />
+    In-Person/Indoor &amp; Live-Streamed Online
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
+  </div>
+</div>
+HTML
+    ),
+    4 => array(
+      'date' => '2021-06-20',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Sunday Worship</strong><br />
+    In-Person/Indoor &amp; Live-Streamed Online
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
+  </div>
+</div>
+HTML
+  ),
+  5 => array(
+    'date' => '2021-06-27',
+    'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Sunday Worship</strong><br />
+    In-Person/Indoor &amp; Live-Streamed Online
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
+  </div>
+</div>
+HTML
+  )
+);
 ?>
