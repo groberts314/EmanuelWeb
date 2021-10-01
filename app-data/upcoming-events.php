a97c15e0b0de0262c0c74ca5da5bdeec211c6936
Geoffrey Roberts <groberts314@yahoo.com>
2021-10-01 13:10:46 -0700

    feat: updates for October 2021
    * Upcoming Events
    * Calendar
    * Newsletter

--- app-data/upcoming-events.php
+++ app-data/upcoming-events.php
@@ -1,12 +1,19 @@
 <?php
   return array(
-  0 => array(
-    'date' => '2021-08-29',
-    'content' => <<<HTML
+    0 => array(
+      'date' => '2021-10-03',
+      'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    <strong>Sunday Worship</strong><br />
-    In-Person/Indoor &amp; Live-Streamed Online
+    <strong>Traditional Worship</strong>
+  </div>
+  <div class="event-time">
+    9 AM
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Sunday School</strong> &ndash; 3 Years thru 6th Grade &ndash; in Moerer Hall
   </div>
   <div class="event-time">
     10 AM
@@ -14,33 +21,28 @@
 </div>
 <div class="event-detail">
   <div class="event-description">
-    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
+    <strong>Praise Worship</strong>
+  </div>
+  <div class="event-time">
+    10:45 AM
   </div>
 </div>
 HTML
-  ),
-  1 => array(
-    'date' => '2021-09-01',
-    'content' => <<<HTML
+    ),
+    1 => array(
+      'date' => '2021-10-10',
+      'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    <a href="/event-fliers/2021-09-01-bible-study.jpg" target="_blank">
-      <strong>Bible Study on Zoom</strong>
-    </a>
+    <strong>Traditional Worship</strong>
   </div>
   <div class="event-time">
     9 AM
   </div>
 </div>
-HTML
-  ),
-  2 => array(
-    'date' => '2021-09-05',
-    'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    <strong>Sunday Worship</strong><br />
-    In-Person/Indoor &amp; Live-Streamed Online
+    <strong>Sunday School</strong> &ndash; 3 Years thru 6th Grade &ndash; in Moerer Hall
   </div>
   <div class="event-time">
     10 AM
@@ -48,38 +50,36 @@ HTML
 </div>
 <div class="event-detail">
   <div class="event-description">
-    <a href="/sunday-school-online"><strong>Sunday School Online</strong></a>
-  </div>
-</div>
-HTML
-  ),
-  3 => array(
-    'date' => '2021-09-08',
-    'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <a href="/event-fliers/2021-09-01-bible-study.jpg" target="_blank">
-      <strong>Bible Study on Zoom</strong>
-    </a>
+    <strong>Praise Worship</strong>
   </div>
   <div class="event-time">
-    9 AM
+    10:45 AM
   </div>
 </div>
 HTML
-  ),
-  4 => array(
-    'date' => '2021-09-12',
-    'content' => <<<HTML
+    ),
+    2 => array(
+      'date' => '2021-10-16',
+      'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    <a href="/event-fliers/2021-09-12-2_worship_services.jpg" target="_blank">
-      <strong>Fall Kick-Off</strong>
+    <a href="/event-fliers/2021-10-16-vicar-matt-ordination.jpg" target="_blank">
+      <strong>Ordination for Vicar Matt Polsdofer</strong>
     </a>
     <br />
-    Return to two worship services and in-person Sunday School re-starting.
+    Immanuel Lutheran Church<br />
+    Forest City, Iowa<br />
+    Livestreamed on Facebook
+  </div>
+  <div class="event-time">
+    11 AM
   </div>
 </div>
+HTML
+    ),
+    3 => array(
+      'date' => '2021-10-17',
+      'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
     <strong>Traditional Worship</strong>
@@ -105,99 +105,27 @@ HTML
   </div>
 </div>
 HTML
-  ),
-  5 => array(
-    'date' => '2021-09-15',
-    'content' => <<<HTML
+    ),
+    4 => array(
+      'date' => '2021-10-18',
+      'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    <a href="/event-fliers/2021-09-01-bible-study.jpg" target="_blank">
-      <strong>Bible Study on Zoom</strong>
+    <a href="/event-fliers/2021-10-18-joy-circle.jpg" target="_blank">
+      <strong>Joy Circle</strong>
     </a>
-  </div>
-  <div class="event-time">
-    9 AM
-  </div>
-</div>
-HTML
-  ),
-  6 => array(
-    'date' => '2021-09-19',
-    'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Traditional Worship</strong>
-  </div>
-  <div class="event-time">
-    9 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Sunday School</strong> &ndash; 3 Years thru 6th Grade &ndash; in Moerer Hall
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Praise Worship</strong>
-  </div>
-  <div class="event-time">
-    10:45 AM
-  </div>
-</div>
-HTML
-  ),
-  7 => array(
-    'date' => '2021-09-20',
-    'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <a href="/event-fliers/2021-09-20-joy-circle.jpg" target="_blank">
-      <strong>Joy Circle Women&rsquo;s Bible Study</strong>
-    </a>
+    <br />
+    Participate in person or via Zoom
   </div>
   <div class="event-time">
     9:30 AM
   </div>
 </div>
 HTML
-  ),
-  8 => array(
-    'date' => '2021-09-21',
-    'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <a href="/event-fliers/2021-09-21-study-buddies.jpg" target="_blank">
-      <strong>Study Buddies Tutoring Resumes</strong>
-    </a>
-  </div>
-  <div class="event-time">
-    3 PM
-  </div>
-</div>
-HTML
-  ),
-  9 => array(
-    'date' => '2021-09-22',
-    'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <a href="/event-fliers/2021-09-01-bible-study.jpg" target="_blank">
-      <strong>Bible Study on Zoom</strong>
-    </a>
-  </div>
-  <div class="event-time">
-    9 AM
-  </div>
-</div>
-HTML
-  ),
-  10 => array(
-    'date' => '2021-09-26',
-    'content' => <<<HTML
+    ),
+    5 => array(
+      'date' => '2021-10-24',
+      'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
     <strong>Traditional Worship</strong>
@@ -223,28 +151,18 @@ HTML
   </div>
 </div>
 HTML
-  ),
-  11 => array(
-    'date' => '2021-09-29',
-    'content' => <<<HTML
+    ),
+    6 => array(
+      'date' => '2021-10-31',
+      'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    <a href="/event-fliers/2021-09-01-bible-study.jpg" target="_blank">
-      <strong>Bible Study on Zoom</strong>
-    </a>
-  </div>
-  <div class="event-time">
-    9 AM
+    <strong>Reformation Sunday</strong>
   </div>
 </div>
-HTML
-  ),
-  12 => array(
-    'date' => '2021-10-03',
-    'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    <strong>Traditional Worship</strong>
+    Traditional Worship
   </div>
   <div class="event-time">
     9 AM
@@ -252,7 +170,7 @@ HTML
 </div>
 <div class="event-detail">
   <div class="event-description">
-    <strong>Sunday School</strong> &ndash; 3 Years thru 6th Grade &ndash; in Moerer Hall
+    Sunday School &ndash; 3 Years thru 6th Grade &ndash; in Moerer Hall
   </div>
   <div class="event-time">
     10 AM
@@ -260,13 +178,13 @@ HTML
 </div>
 <div class="event-detail">
   <div class="event-description">
-    <strong>Praise Worship</strong>
+    Praise Worship
   </div>
   <div class="event-time">
     10:45 AM
   </div>
 </div>
 HTML
-  )
-);
+    )
+  );
 ?>
