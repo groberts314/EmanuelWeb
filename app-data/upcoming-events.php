8706c7bd929420246ea0b65b4136822ea143a669
Geoffrey Roberts <groberts314@yahoo.com>
2021-11-07 06:19:19 -0800

    feat: updates for November 2021
    * Upcoming Events
    * Calendar
    * Newsletter

--- app-data/upcoming-events.php
+++ app-data/upcoming-events.php
@@ -1,7 +1,7 @@
 <?php
   return array(
     0 => array(
-      'date' => '2021-10-03',
+      'date' => '2021-11-07',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
@@ -30,7 +30,46 @@
 HTML
     ),
     1 => array(
-      'date' => '2021-10-10',
+      'date' => '2021-11-14',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Traditional Worship</strong>
+  </div>
+  <div class="event-time">
+    9 AM
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Sunday School</strong> &ndash; 3 Years thru 6th Grade &ndash; in Moerer Hall
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Praise Worship</strong>
+  </div>
+  <div class="event-time">
+    10:45 AM
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <a href="/event-fliers/2021-11-14-annual-meeting.jpg" target="_blank">
+      <strong>Annual Congregational Meeting</strong>
+    </a>
+  </div>
+  <div class="event-time">
+    4:30 PM
+  </div>
+</div>
+HTML
+    ),
+    2 => array(
+      'date' => '2021-11-21',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
@@ -58,31 +97,17 @@ HTML
 </div>
 HTML
     ),
-    2 => array(
-      'date' => '2021-10-16',
+    3 => array(
+      'date' => '2021-11-28',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    <a href="/event-fliers/2021-10-16-vicar-matt-ordination.jpg" target="_blank">
-      <strong>Ordination for Vicar Matt Polsdofer</strong>
-    </a>
-    <br />
-    Immanuel Lutheran Church<br />
-    Forest City, Iowa<br />
-    Livestreamed on Facebook
-  </div>
-  <div class="event-time">
-    11 AM
+    <strong>First Sunday of Advent</strong>
   </div>
 </div>
-HTML
-    ),
-    3 => array(
-      'date' => '2021-10-17',
-      'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    <strong>Traditional Worship</strong>
+    Traditional Worship
   </div>
   <div class="event-time">
     9 AM
@@ -90,7 +115,7 @@ HTML
 </div>
 <div class="event-detail">
   <div class="event-description">
-    <strong>Sunday School</strong> &ndash; 3 Years thru 6th Grade &ndash; in Moerer Hall
+    Sunday School &ndash; 3 Years thru 6th Grade &ndash; in Moerer Hall
   </div>
   <div class="event-time">
     10 AM
@@ -98,7 +123,7 @@ HTML
 </div>
 <div class="event-detail">
   <div class="event-description">
-    <strong>Praise Worship</strong>
+    Praise Worship
   </div>
   <div class="event-time">
     10:45 AM
@@ -107,67 +132,11 @@ HTML
 HTML
     ),
     4 => array(
-      'date' => '2021-10-18',
+      'date' => '2021-12-05',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    <a href="/event-fliers/2021-10-18-joy-circle.jpg" target="_blank">
-      <strong>Joy Circle</strong>
-    </a>
-    <br />
-    Participate in person or via Zoom
-  </div>
-  <div class="event-time">
-    9:30 AM
-  </div>
-</div>
-HTML
-    ),
-    5 => array(
-      'date' => '2021-10-24',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Traditional Worship</strong>
-  </div>
-  <div class="event-time">
-    9 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Sunday School</strong> &ndash; 3 Years thru 6th Grade &ndash; in Moerer Hall
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Praise Worship</strong>
-  </div>
-  <div class="event-time">
-    10:45 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <a href="/event-fliers/2021-10-24_trunk_or_treat.pdf" target="_blank">
-      <strong>Trunk-or-Treat</strong>
-    </a>
-  </div>
-  <div class="event-time">
-    6:30 PM
-  </div>
-</div>
-HTML
-    ),
-    6 => array(
-      'date' => '2021-10-31',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Reformation Sunday</strong>
+    <strong>Second Sunday of Advent</strong>
   </div>
 </div>
 <div class="event-detail">
