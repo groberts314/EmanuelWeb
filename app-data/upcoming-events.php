4cd05fec3b5c9f19d4cc1c6007802a9b0176bd51
Geoffrey Roberts <groberts314@yahoo.com>
2021-11-29 15:50:42 -0800

    feat: updates for December 2021
    * Upcoming Events
    * Calendar
    * Newsletter

--- app-data/upcoming-events.php
+++ app-data/upcoming-events.php
@@ -1,137 +1,6 @@
 <?php
   return array(
     0 => array(
-      'date' => '2021-11-07',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Traditional Worship</strong>
-  </div>
-  <div class="event-time">
-    9 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Sunday School</strong> &ndash; 3 Years thru 6th Grade &ndash; in Moerer Hall
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Praise Worship</strong>
-  </div>
-  <div class="event-time">
-    10:45 AM
-  </div>
-</div>
-HTML
-    ),
-    1 => array(
-      'date' => '2021-11-14',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Traditional Worship</strong>
-  </div>
-  <div class="event-time">
-    9 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Sunday School</strong> &ndash; 3 Years thru 6th Grade &ndash; in Moerer Hall
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Praise Worship</strong>
-  </div>
-  <div class="event-time">
-    10:45 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <a href="/event-fliers/2021-11-14-annual-meeting.jpg" target="_blank">
-      <strong>Annual Congregational Meeting</strong>
-    </a>
-  </div>
-  <div class="event-time">
-    4:30 PM
-  </div>
-</div>
-HTML
-    ),
-    2 => array(
-      'date' => '2021-11-21',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Traditional Worship</strong>
-  </div>
-  <div class="event-time">
-    9 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Sunday School</strong> &ndash; 3 Years thru 6th Grade &ndash; in Moerer Hall
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Praise Worship</strong>
-  </div>
-  <div class="event-time">
-    10:45 AM
-  </div>
-</div>
-HTML
-    ),
-    3 => array(
-      'date' => '2021-11-28',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>First Sunday of Advent</strong>
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    Traditional Worship
-  </div>
-  <div class="event-time">
-    9 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    Sunday School &ndash; 3 Years thru 6th Grade &ndash; in Moerer Hall
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    Praise Worship
-  </div>
-  <div class="event-time">
-    10:45 AM
-  </div>
-</div>
-HTML
-    ),
-    4 => array(
       'date' => '2021-12-05',
       'content' => <<<HTML
 <div class="event-detail">
@@ -141,15 +10,7 @@ HTML
 </div>
 <div class="event-detail">
   <div class="event-description">
-    Traditional Worship
-  </div>
-  <div class="event-time">
-    9 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    Sunday School &ndash; 3 Years thru 6th Grade &ndash; in Moerer Hall
+    Combined Worship
   </div>
   <div class="event-time">
     10 AM
@@ -157,10 +18,87 @@ HTML
 </div>
 <div class="event-detail">
   <div class="event-description">
-    Praise Worship
+    <a href="event-fliers/2021-12-05-special-ministry-celebration.jpg" target="_blank">
+      Special Ministry Celebration
+    </a>
   </div>
   <div class="event-time">
-    10:45 AM
+    11 AM
+  </div>
+</div>
+HTML
+    ),
+    1 => array(
+      'date' => '2021-12-12',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Third Sunday of Advent</strong>
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    Combined Worship
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+HTML
+    ),
+    2 => array(
+      'date' => '2021-12-19',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Fourth Sunday of Advent</strong>
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    Combined Worship
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+HTML
+    ),
+    3 => array(
+      'date' => '2021-12-24',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Christams Eve Worship</strong>
+  </div>
+  <div class="event-time">
+    7 PM
+  </div>
+</div>
+HTML
+    ),
+    4 => array(
+      'date' => '2021-12-26',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    Combined Worship
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+HTML
+    ),
+    5 => array(
+      'date' => '2022-01-02',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    Combined Worship
+  </div>
+  <div class="event-time">
+    10 AM
   </div>
 </div>
 HTML
