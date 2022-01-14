9db329b2b757bb5bed2e2904e59389fa4e1a2273
Geoffrey Roberts <groberts314@yahoo.com>
2022-01-14 03:30:36 -0800

    feat: add new "Special Events" live video page

---
+++ special-events.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php require_once('./partials/head-metas.phtml') ?>
+  <title>Special Events Live Video | Emanuel Evangelical Lutheran Church</title>
+  <?php require_once('./partials/head-includes.phtml') ?>
+</head>
+
+<body>
+  <div class="container layout-v2">
+    <?php require_once('./partials/masthead-top-nav.phtml') ?>
+
+    <div class="row main-content inner-page-content">
+      <div class="col-xs-12 col-sm-8">
+        <h1 class="page-title">Special Events Live Video</h1>
+        <div class="row">
+          <div class="col-xs-12">
+            <div class="fluid-width-video-wrapper">
+              <iframe class="video-iframe" src="https://vimeo.com/event/1721381/embed" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-4 photo-right no-btm-margin">
+        <img src="images/who-we-are/emanuel-sign.jpg" alt="Emanuel: God With Us" />
+      </div>
+    </div>
+
+    <?php require_once('./partials/footer.phtml') ?>
+  </div>
+
+  <?php require_once('./partials/footer-includes.phtml') ?>
+</body>
+
+</html>
