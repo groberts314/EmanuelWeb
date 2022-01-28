989a525fa257114b5ff3255fd70e769aedbdfebd
Geoffrey Roberts <groberts314@yahoo.com>
2022-01-28 04:10:48 -0800

    feat: updates for February 2022
    * Upcoming Events
    * Calendar
    * Newsletter
      * Established archive section for 2022 =)
      * Added some comments and `id` attributes on the panels for each year to help maintainability
      * TODO: Create a partial view for this!

--- app-data/upcoming-events.php
+++ app-data/upcoming-events.php
@@ -1,183 +1,6 @@
 <?php
   return array(
     0 => array(
-      'date' => '2022-01-06',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <a href="event-fliers/2022-01-06-book-club.jpg" target="_blank">
-      Zoom Book Study
-    </a>
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-HTML
-    ),
-    1 => array(
-      'date' => '2022-01-09',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Traditional Worship</strong>
-  </div>
-  <div class="event-time">
-    9 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Sunday School</strong> &ndash; 3 Years thru 6th Grade &ndash; in Moerer Hall
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Praise Worship</strong>
-  </div>
-  <div class="event-time">
-    10:45 AM
-  </div>
-</div>
-HTML
-    ),
-    2 => array(
-      'date' => '2022-01-13',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <a href="event-fliers/2022-01-06-book-club.jpg" target="_blank">
-      Zoom Book Study
-    </a>
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-HTML
-    ),
-    3 => array(
-      'date' => '2022-01-16',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Traditional Worship</strong>
-  </div>
-  <div class="event-time">
-    9 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Sunday School</strong> &ndash; 3 Years thru 6th Grade &ndash; in Moerer Hall
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Praise Worship</strong>
-  </div>
-  <div class="event-time">
-    10:45 AM
-  </div>
-</div>
-HTML
-    ),
-    4 => array(
-      'date' => '2022-01-17',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <a href="event-fliers/2022-01-17-joy-circle.jpg" target="_blank">
-      Joy Circle Women&rsquo;s Bible Study
-    </a>
-  </div>
-  <div class="event-time">
-    9:30 AM
-  </div>
-</div>
-HTML
-    ),
-    5 => array(
-      'date' => '2022-01-20',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <a href="event-fliers/2022-01-06-book-club.jpg" target="_blank">
-      Zoom Book Study
-    </a>
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-HTML
-    ),
-    6 => array(
-      'date' => '2022-01-23',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Traditional Worship</strong>
-  </div>
-  <div class="event-time">
-    9 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Sunday School</strong> &ndash; 3 Years thru 6th Grade &ndash; in Moerer Hall
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Praise Worship</strong>
-  </div>
-  <div class="event-time">
-    10:45 AM
-  </div>
-</div>
-HTML
-    ),
-    7 => array(
-      'date' => '2022-01-24',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <a href="event-fliers/2022-01-24-blood-drive.jpg" target="_blank">
-      Blood Drive
-    </a>
-  </div>
-  <div class="event-time">
-    12 PM
-  </div>
-</div>
-HTML
-    ),
-    8 => array(
-      'date' => '2022-01-27',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <a href="event-fliers/2022-01-06-book-club.jpg" target="_blank">
-      Zoom Book Study
-    </a>
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-HTML
-    ),
-    9 => array(
       'date' => '2022-01-30',
       'content' => <<<HTML
 <div class="event-detail">
@@ -206,7 +29,7 @@ HTML
 </div>
 HTML
     ),
-    10 => array(
+    1 => array(
       'date' => '2022-02-03',
       'content' => <<<HTML
 <div class="event-detail">
@@ -221,8 +44,164 @@ HTML
 </div>
 HTML
     ),
-    11 => array(
+    2 => array(
       'date' => '2022-02-06',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Traditional Worship</strong>
+  </div>
+  <div class="event-time">
+    9 AM
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Sunday School</strong> &ndash; 3 Years thru 6th Grade &ndash; in Moerer Hall
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Praise Worship</strong>
+  </div>
+  <div class="event-time">
+    10:45 AM
+  </div>
+</div>
+HTML
+    ),
+    3 => array(
+      'date' => '2022-02-13',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Traditional Worship</strong>
+  </div>
+  <div class="event-time">
+    9 AM
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Sunday School</strong> &ndash; 3 Years thru 6th Grade &ndash; in Moerer Hall
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Praise Worship</strong>
+  </div>
+  <div class="event-time">
+    10:45 AM
+  </div>
+</div>
+HTML
+    ),
+    4 => array(
+      'date' => '2022-02-19',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <a href="event-fliers/2022-02-19-transition-team.jpg" target="_blank">
+      Emanuel Heritage and History Event
+    </a>
+  </div>
+  <div class="event-time">
+    1 PM
+  </div>
+</div>
+HTML
+    ),
+    5 => array(
+      'date' => '2022-02-20',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Traditional Worship</strong>
+  </div>
+  <div class="event-time">
+    9 AM
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Sunday School</strong> &ndash; 3 Years thru 6th Grade &ndash; in Moerer Hall
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Praise Worship</strong>
+  </div>
+  <div class="event-time">
+    10:45 AM
+  </div>
+</div>
+HTML
+    ),
+    6 => array(
+      'date' => '2022-02-21',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <a href="event-fliers/2022-02-21-joy-circle.jpg" target="_blank">
+      Joy Circle Women&rsquo;s Bible Study
+    </a>
+  </div>
+  <div class="event-time">
+    9:30 AM
+  </div>
+</div>
+HTML
+    ),
+    7 => array(
+      'date' => '2022-02-27',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Traditional Worship</strong>
+  </div>
+  <div class="event-time">
+    9 AM
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Sunday School</strong> &ndash; 3 Years thru 6th Grade &ndash; in Moerer Hall
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Praise Worship</strong>
+  </div>
+  <div class="event-time">
+    10:45 AM
+  </div>
+</div>
+HTML
+    ),
+    8 => array(
+      'date' => '2022-03-02',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Ash Wednesday</strong>
+  </div>
+</div>
+HTML
+    ),
+    9 => array(
+      'date' => '2022-03-06',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
--- newsletter.php
+++ newsletter.php
@@ -18,29 +18,54 @@
           <div class="newsletter-issue-list">
             <h2 class="section-title no-top-margin">Current Issue</h2>
             <p>
-              <a href="../newsletters/newsletter-2022-01.pdf" target="_blank">January 2022</a>
+              <a href="../newsletters/newsletter-2022-02.pdf" target="_blank">February 2022</a>
             </p>
           </div>
           <div class="newsletter-current-issue-thumbnail-mobile hidden-md hidden-lg">
-            <img src="images/newsletter/newsletter-2022-01.jpg" alt="The Good News: January 2022" />
+            <img src="images/newsletter/newsletter-2022-02.jpg" alt="The Good News: February 2022" />
           </div>
         </div>
       </div>
       <div class="col-xs-12 col-md-4 photo-right no-btm-margin hidden-xs hidden-sm">
-        <img src="images/newsletter/newsletter-2022-01.jpg" alt="The Good News: January 2022" />
+        <img src="images/newsletter/newsletter-2022-02.jpg" alt="The Good News: February 2022" />
       </div>
       <div class="col-xs-12">
         <div class="newsletter-additional-issues-list">
           <div class="panel-group" id="newsletter-archive-accordion" role="tablist" aria-multiselectable="true">
-            <div class="panel panel-default">
+            <!-- 2022 -->
+            <div class="panel panel-default" id="newsletter-archive-panel-2022">
+              <div class="panel-heading" role="tab" id="heading-2022">
+                <h4 class="panel-title">
+                <a role="button" data-toggle="collapse" data-parent="#newsletter-archive-accordion" href="#collapse-2022" aria-expanded="true" aria-controls="collapse-2022">
+                  2022
+                </a>
+                </h4>
+              </div>
+              <div id="collapse-2022" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="heading-2022">
+                <div class="panel-body">
+                  <div class="newsletter-year-issue-list">
+                    <ul class="list-group">
+                      <li class="list-group-item" data-thumbnail="images/newsletter/newsletter-2022-01.jpg" data-target="#thumbnail-2022">
+                        <a href="../newsletters/newsletter-2022-01.pdf" target="_blank">January 2022</a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="newsletter-year-issue-thumbnail">
+                    <img id="thumbnail-2022" src="images/newsletter/newsletter-2022-01.jpg" alt="The Good News: 2022" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- 2021 -->
+            <div class="panel panel-default" id="newsletter-archive-panel-2021">
               <div class="panel-heading" role="tab" id="heading-2021">
                 <h4 class="panel-title">
-                <a role="button" data-toggle="collapse" data-parent="#newsletter-archive-accordion" href="#collapse-2021" aria-expanded="true" aria-controls="collapse-2021">
+                <a role="button" data-toggle="collapse" data-parent="#newsletter-archive-accordion" href="#collapse-2021" aria-expanded="false" aria-controls="collapse-2021">
                   2021
                 </a>
                 </h4>
               </div>
-              <div id="collapse-2021" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="heading-2021">
+              <div id="collapse-2021" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading-2021">
                 <div class="panel-body">
                   <div class="newsletter-year-issue-list">
                     <ul class="list-group">
@@ -88,7 +113,8 @@
                 </div>
               </div>
             </div>
-            <div class="panel panel-default">
+            <!-- 2020 -->
+            <div class="panel panel-default" id="newsletter-archive-panel-2020">
               <div class="panel-heading" role="tab" id="heading-2020">
                 <h4 class="panel-title">
                 <a role="button" data-toggle="collapse" data-parent="#newsletter-archive-accordion" href="#collapse-2020" aria-expanded="false" aria-controls="collapse-2020">
@@ -144,7 +170,8 @@
                 </div>
               </div>
             </div>
-            <div class="panel panel-default">
+            <!-- 2019 -->
+            <div class="panel panel-default" id="newsletter-archive-panel-2019">
               <div class="panel-heading" role="tab" id="heading-2019">
                 <h4 class="panel-title">
                 <a class="collapsed" role="button" data-toggle="collapse" data-parent="#newsletter-archive-accordion" href="#collapse-2019" aria-expanded="false" aria-controls="collapse-2019">
@@ -200,7 +227,8 @@
                 </div>
               </div>
             </div>
-            <div class="panel panel-default">
+            <!-- 2018 -->
+            <div class="panel panel-default" id="newsletter-archive-panel-2018">
               <div class="panel-heading" role="tab" id="heading-2018">
                 <h4 class="panel-title">
                 <a class="collapsed" role="button" data-toggle="collapse" data-parent="#newsletter-archive-accordion" href="#collapse-2018" aria-expanded="false" aria-controls="collapse-2018">
