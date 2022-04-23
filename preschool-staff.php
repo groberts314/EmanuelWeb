213f6c48171c2704c7f69a24448702e999710c5f
Geoffrey Roberts <groberts314@yahoo.com>
2022-04-23 09:04:31 -0700

    feat: remove Ms. Priscilla from Preschool Staff Page

--- preschool-staff.php
+++ preschool-staff.php
@@ -40,13 +40,6 @@
     <div class="row main-content inner-page-content">
       <div class="col-xs-12 col-sm-4 photo-left">
         <h2 class="staff-title no-top-margin no-btm-margin">
-          Ms. Priscilla<br />
-          Teacher
-        </h2>
-        <img src="images/preschool-staff/priscilla.jpg" alt="Ms. Priscilla, Teacher" />
-      </div>
-      <div class="col-xs-12 col-sm-4 photo-left">
-        <h2 class="staff-title no-top-margin no-btm-margin">
           Ms. Tanya<br />
           Teacher
         </h2>
