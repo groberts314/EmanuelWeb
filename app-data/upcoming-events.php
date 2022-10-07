37ebec9f28f31e9147af0bdf33f36fc1162f1f07
Geoffrey Roberts <groberts314@yahoo.com>
2022-10-07 16:21:57 -0700

    feat: updates for October 2022
    * Upcoming Events
    * Calendar
    * Newsletter

--- app-data/upcoming-events.php
+++ app-data/upcoming-events.php
@@ -1,7 +1,7 @@
  <?php
   return array(
     0 => array(
-      'date' => '2022-09-25',
+      'date' => '2022-10-09',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
@@ -30,7 +30,7 @@
 HTML
     ),
     1 => array(
-      'date' => '2022-10-02',
+      'date' => '2022-10-16',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
@@ -59,7 +59,80 @@ HTML
 HTML
     ),
     2 => array(
-      'date' => '2022-10-09',
+      'date' => '2022-10-17',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <a href="event-fliers/2022-10-17-joy-circle.pdf" target="_blank">
+      Joy Circle Women&rsquo;s Bible Study
+    </a>
+  </div>
+  <div class="event-time">
+    9:30 AM
+  </div>
+</div>
+HTML
+    ),
+    3 => array(
+      'date' => '2022-10-23',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    Traditional Worship
+  </div>
+  <div class="event-time">
+    9 AM
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    Sunday School
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    Praise (Contemporary) Worship
+  </div>
+  <div class="event-time">
+    10:45 AM
+  </div>
+</div>
+HTML
+    ),
+    4 =>  array(
+      'date' => '2022-10-30',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    Traditional Worship
+  </div>
+  <div class="event-time">
+    9 AM
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    Sunday School
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    Praise (Contemporary) Worship
+  </div>
+  <div class="event-time">
+    10:45 AM
+  </div>
+</div>
+HTML
+    ),
+    5 => array(
+      'date' => '2022-11-06',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
