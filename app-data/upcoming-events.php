fab86a08b3cdc606f7f438211ad4bde52cfe9bcb
Geoffrey Roberts <groberts314@yahoo.com>
2023-04-24 09:29:42 -0700

    feat: update Upcoming Events
    One combined worship, followed by potluck on Sunday April 30

--- app-data/upcoming-events.php
+++ app-data/upcoming-events.php
@@ -1,45 +1,11 @@
  <?php
   return array(
     0 => array(
-      'date' => '2023-04-09',
+      'date' => '2023-04-30',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    <a href="event-fliers/2023-holy-week-and-easter.pdf" target="_blank">
-      <strong>
-        Easter Sunday<br />
-        HE Is Risen Indeed!  Alleluia!
-      </strong>
-    </a>
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    Sunrise Worship
-  </div>
-  <div class="event-time">
-    6:30 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    &ldquo;Continental&rdquo; Easter Breakfast
-  </div>
-  <div class="event-time">
-    7:15 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    Traditional Worship
-  </div>
-  <div class="event-time">
-    9 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    Sunday School
+    Combined Worship
   </div>
   <div class="event-time">
     10 AM
@@ -47,147 +13,15 @@
 </div>
 <div class="event-detail">
   <div class="event-description">
-    Praise (Contemporary) Worship
+    Potluck
   </div>
   <div class="event-time">
-    10:45 AM
+    11 AM
   </div>
 </div>
 HTML
     ),
     1 => array(
-      'date' => '2023-04-14',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <a href="event-fliers/2023-04-14-ladies-lunch.pdf" target="_blank">
-      <strong>What&rsquo;s 4 Lunch Ladies?!</strong>
-    </a>
-  </div>
-  <div class="event-time">
-    11:30 AM
-  </div>
-</div>
-HTML
-    ),
-    2 => array(
-      'date' => '2023-04-16',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    Traditional Worship
-  </div>
-  <div class="event-time">
-    9 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    Sunday School
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    Praise (Contemporary) Worship
-  </div>
-  <div class="event-time">
-    10:45 AM
-  </div>
-</div>
-HTML
-    ),
-    3 => array(
-      'date' => '2023-04-17',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <a href="event-fliers/2023-04-17-joy-circle.pdf" target="_blank">
-      <strong>Joy Circle Women&rsquo;s Bible Study</strong>  
-    </a>
-  </div>
-  <div class="event-time">
-    9:30 AM
-  </div>
-</div>
-HTML
-    ),
-    4 => array(
-      'date' => '2023-04-20',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <a href="event-fliers/2023-04-20-book-study.pdf" target="_blank">
-      <strong>Book Study (in-person and Zoom)</strong>
-    </a>
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-HTML
-    ),
-    5 => array(
-      'date' => '2023-04-23',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    Traditional Worship
-  </div>
-  <div class="event-time">
-    9 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    Sunday School
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    Praise (Contemporary) Worship
-  </div>
-  <div class="event-time">
-    10:45 AM
-  </div>
-</div>
-HTML
-    ),
-    6 => array(
-      'date' => '2023-04-30',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    Traditional Worship
-  </div>
-  <div class="event-time">
-    9 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    Sunday School
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    Praise (Contemporary) Worship
-  </div>
-  <div class="event-time">
-    10:45 AM
-  </div>
-</div>
-HTML
-    ),
-    7 => array(
       'date' => '2023-05-07',
       'content' => <<<HTML
 <div class="event-detail">
