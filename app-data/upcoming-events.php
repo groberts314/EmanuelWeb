d540f833d993aff7633c8ec2080c2b8bea114033
Geoffrey Roberts <groberts314@yahoo.com>
2023-07-08 02:28:08 -0700

    feat: Upcoming Events for July 2023

--- app-data/upcoming-events.php
+++ app-data/upcoming-events.php
@@ -1,54 +1,99 @@
  <?php
   return array(
     0 => array(
-      'date' => '2023-04-30',
+      'date' => '2023-07-09',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    Combined Worship
+    Combined Worship Service
   </div>
   <div class="event-time">
     10 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    Potluck
-  </div>
-  <div class="event-time">
-    11 AM
   </div>
 </div>
 HTML
     ),
     1 => array(
-      'date' => '2023-05-07',
+      'date' => '2023-07-14',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    Traditional Worship
+    <a href="event-fliers/2023-07-14-ladies-lunch.pdf" target="_blank">
+      <strong>What&rsquo;s 4 Lunch Ladies?</strong>
+    </a>
   </div>
   <div class="event-time">
-    9 AM
+    11:30 AM
   </div>
 </div>
+HTML
+    ),
+    2 => array(
+      'date' => '2023-07-16',
+      'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    Sunday School
+    Combined Worship Service
   </div>
   <div class="event-time">
     10 AM
   </div>
 </div>
+HTML
+    ),
+    3 => array(
+      'date' => '2023-07-17',
+      'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    Praise (Contemporary) Worship
+    <a href="event-fliers/2023-07-17-joy-circle.pdf" target="_blank">
+      <strong>Joy Circle Women&rsquo;s Bible Study</strong>  
+    </a>
   </div>
   <div class="event-time">
-    10:45 AM
+    9:30 AM
   </div>
 </div>
 HTML
-    )
+    ),
+    4 => array(
+      'date' => '2023-07-23',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    Combined Worship Service
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+HTML
+    ),
+    5 => array(
+      'date' => '2023-07-30',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    Combined Worship Service
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+HTML
+    ),
+    6 => array(
+      'date' => '2023-08-06',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    Combined Worship Service
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+HTML
+    )    
   );
 ?>
