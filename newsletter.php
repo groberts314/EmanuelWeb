8472727a30291a721fc2954883391e38659d12bc
Geoffrey Roberts <groberts314@yahoo.com>
2023-07-08 01:46:00 -0700

    feat: update newsletter Page for July 2023

--- newsletter.php
+++ newsletter.php
@@ -18,16 +18,16 @@
           <div class="newsletter-issue-list">
             <h2 class="section-title no-top-margin">Current Issue</h2>
             <p>
-              <a href="../newsletters/newsletter-2023-06.pdf" target="_blank">June 2023</a>
+              <a href="../newsletters/newsletter-2023-07.pdf" target="_blank">July 2023</a>
             </p>
           </div>
           <div class="newsletter-current-issue-thumbnail-mobile hidden-md hidden-lg">
-            <img src="images/newsletter/newsletter-2023-06.jpg" alt="The Good News: June 2023" />
+            <img src="images/newsletter/newsletter-2023-07.jpg" alt="The Good News: July 2023" />
           </div>
         </div>
       </div>
       <div class="col-xs-12 col-md-4 photo-right no-btm-margin hidden-xs hidden-sm">
-        <img src="images/newsletter/newsletter-2023-06.jpg" alt="The Good News: June 2023" />
+        <img src="images/newsletter/newsletter-2023-07.jpg" alt="The Good News: July 2023" />
       </div>
       <div class="col-xs-12">
         <div class="newsletter-additional-issues-list">
@@ -45,7 +45,10 @@
                 <div class="panel-body">
                   <div class="newsletter-year-issue-list">
                     <ul class="list-group">
-                    <li class="list-group-item" data-thumbnail="images/newsletter/newsletter-2023-05.jpg" data-target="#thumbnail-2023">
+                      <li class="list-group-item" data-thumbnail="images/newsletter/newsletter-2023-06.jpg" data-target="#thumbnail-2023">
+                        <a href="../newsletters/newsletter-2023-06.pdf" target="_blank">June 2023</a>
+                      </li>
+                      <li class="list-group-item" data-thumbnail="images/newsletter/newsletter-2023-05.jpg" data-target="#thumbnail-2023">
                         <a href="../newsletters/newsletter-2023-05.pdf" target="_blank">May 2023</a>
                       </li>
                       <li class="list-group-item" data-thumbnail="images/newsletter/newsletter-2023-04.jpg" data-target="#thumbnail-2023">
@@ -63,7 +66,7 @@
                     </ul>
                   </div>
                   <div class="newsletter-year-issue-thumbnail">
-                    <img id="thumbnail-2023" src="images/newsletter/newsletter-2023-05.jpg" alt="The Good News: 2023" />
+                    <img id="thumbnail-2023" src="images/newsletter/newsletter-2023-06.jpg" alt="The Good News: 2023" />
                   </div>
                 </div>
               </div>
