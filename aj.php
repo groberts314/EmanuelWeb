a4239871fbde9263396475b6be660bc19bf1f985
Geoffrey Roberts <groberts314@yahoo.com>
2023-08-06 11:11:42 -0700

    feat: add AJ's page

---
+++ aj.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php require_once('./partials/head-metas.phtml') ?>
+  <title>AJ's Page | Emanuel Evangelical Lutheran Church</title>
+  <?php require_once('./partials/head-includes.phtml') ?>
+</head>
+
+<body>
+  <div class="container layout-v2">
+    <?php require_once('./partials/masthead-top-nav.phtml') ?>
+
+    <div class="row main-content inner-page-content">
+      <div class="col-xs-12 col-sm-8">
+        <h1 class="page-title">AJ's Page</h1>
+        <div class="row">
+          <div class="col-xs-12">
+            Add stuff here...
+          </div>
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-4 photo-right no-btm-margin">
+        <img src="images/who-we-are/emanuel-sign.jpg" alt="Emanuel: God With Us" />
+      </div>
+    </div>
+
+    <?php require_once('./partials/footer.phtml') ?>
+  </div>
+
+  <?php require_once('./partials/footer-includes.phtml') ?>
+</body>
+
+</html>
