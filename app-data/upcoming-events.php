09be06b2ab97e4d37d61f5d89a349e8800cc5d94
Geoffrey Roberts <groberts314@yahoo.com>
2023-09-13 13:50:14 -0700

    feat: updates for September 2023
    * Homepage Upcoming Events
    * Newsletter
    * Calendar

--- app-data/upcoming-events.php
+++ app-data/upcoming-events.php
@@ -1,11 +1,11 @@
  <?php
   return array(
     0 => array(
-      'date' => '2023-08-06',
+      'date' => '2023-09-17',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    Combined Worship Service
+    Worship Service
   </div>
   <div class="event-time">
     10 AM
@@ -14,52 +14,11 @@
 HTML
     ),
     1 => array(
-      'date' => '2023-08-11',
+      'date' => '2023-09-18',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    <a href="event-fliers/2023-08-11-whats-4-lunch-ladies.jpg" target="_blank">
-      <strong>What&rsquo;s 4 Lunch Ladies?</strong>
-    </a>
-  </div>
-  <div class="event-time">
-    11:30 AM
-  </div>
-</div>
-HTML
-    ),
-    2 => array(
-      'date' => '2023-08-13',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    Combined Worship Service
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-HTML
-    ),
-    3 => array(
-      'date' => '2023-08-20',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    Combined Worship Service
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-HTML
-    ),
-    4 => array(
-      'date' => '2023-08-21',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <a href="event-fliers/2023-08-21-joy-circle.jpg" target="_blank">
+    <a href="event-fliers/2023-09-18-joy-circle.jpg" target="_blank">
       <strong>Joy Circle Women&rsquo;s Bible Study</strong>  
     </a>
   </div>
@@ -69,60 +28,36 @@ HTML
 </div>
 HTML
     ),
-    5 => array(
-      'date' => '2023-08-27',
+    2 => array(
+      'date' => '2023-09-24',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    Combined Worship Service
+    Worship Service
   </div>
   <div class="event-time">
     10 AM
   </div>
 </div>
-HTML
-    ),
-    6 => array(
-      'date' => '2023-09-03',
-      'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    Combined Worship Service
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-HTML
-    ),
-    7 => array(
-      'date' => '2023-09-10',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    Combined Worship Service
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-HTML
-    ),
-    8 => array(
-      'date' => '2023-09-12',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <a href="event-fliers/2023-09-12-study-buddies-resumes.jpg" target="_blank">
-      <strong>Study Buddies Tutoring resumes</strong>  
+    <a href="event-fliers/2023-09-24-potluck.jpg" target="_blank">
+      <strong>Potluck</strong>
     </a>
-    <br />
-    <a href="event-fliers/2023-09-12-study-buddies-donations.jpg" target="_blank">
-      See how you can donate
-    </a>
+    welcoming Bishop Emeritus Murray Finck after service.
+  </div>
+</div>
+HTML
+    ),
+    3 => array(
+      'date' => '2023-10-01',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    Worship Service
   </div>
   <div class="event-time">
-    3 PM
+    10 AM
   </div>
 </div>
 HTML
