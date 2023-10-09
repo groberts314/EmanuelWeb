dc31373a2653bb32213ba3997f9009b142f76429
Geoffrey Roberts <groberts314@yahoo.com>
2023-10-09 10:17:12 -0700

    feat: updates for October 2023
    * Homepage Upcoming Events
    * Newsletter
    * Calendar
    * Update styles to make top row modules smaller on Homepage, due to reduced amount of content in the Worship Services area with the consolidation down to one worship service every Sunday.

--- app-data/upcoming-events.php
+++ app-data/upcoming-events.php
@@ -1,7 +1,52 @@
  <?php
   return array(
     0 => array(
-      'date' => '2023-09-17',
+      'date' => '2023-10-10',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <a href="event-fliers/2023-10-10-study-buddies.pdf" target="_blank">
+      <strong>Study Buddies</strong>  
+    </a>
+  </div>
+  <div class="event-time">
+    3 PM
+  </div>
+</div>
+HTML
+    ),
+    1 => array(
+      'date' => '2023-10-11',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <a href="event-fliers/2023-10-11-bible-study.pdf" target="_blank">
+      <strong>Bible Study</strong>  
+    </a>
+  </div>
+  <div class="event-time">
+    9 AM
+  </div>
+</div>
+HTML
+    ),
+    2 => array(
+      'date' => '2023-10-12',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <a href="event-fliers/2023-10-10-study-buddies.pdf" target="_blank">
+      <strong>Study Buddies</strong>  
+    </a>
+  </div>
+  <div class="event-time">
+    3 PM
+  </div>
+</div>
+HTML
+    ),
+    3 => array(
+      'date' => '2023-10-15',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
@@ -13,12 +58,12 @@
 </div>
 HTML
     ),
-    1 => array(
-      'date' => '2023-09-18',
+    4 => array(
+      'date' => '2023-10-16',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    <a href="event-fliers/2023-09-18-joy-circle.jpg" target="_blank">
+    <a href="event-fliers/2023-10-16-joy-circle.pdf" target="_blank">
       <strong>Joy Circle Women&rsquo;s Bible Study</strong>  
     </a>
   </div>
@@ -28,8 +73,53 @@ HTML
 </div>
 HTML
     ),
-    2 => array(
-      'date' => '2023-09-24',
+    5 => array(
+      'date' => '2023-10-17',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <a href="event-fliers/2023-10-10-study-buddies.pdf" target="_blank">
+      <strong>Study Buddies</strong>  
+    </a>
+  </div>
+  <div class="event-time">
+    3 PM
+  </div>
+</div>
+HTML
+    ),
+    6 => array(
+      'date' => '2023-10-18',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <a href="event-fliers/2023-10-11-bible-study.pdf" target="_blank">
+      <strong>Bible Study</strong>  
+    </a>
+  </div>
+  <div class="event-time">
+    9 AM
+  </div>
+</div>
+HTML
+    ),
+    7 => array(
+      'date' => '2023-10-19',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <a href="event-fliers/2023-10-10-study-buddies.pdf" target="_blank">
+      <strong>Study Buddies</strong>  
+    </a>
+  </div>
+  <div class="event-time">
+    3 PM
+  </div>
+</div>
+HTML
+    ),
+    8 => array(
+      'date' => '2023-10-22',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
@@ -41,20 +131,114 @@ HTML
 </div>
 <div class="event-detail">
   <div class="event-description">
-    <a href="event-fliers/2023-09-24-potluck.jpg" target="_blank">
-      <strong>Potluck</strong>
+    <a href="event-fliers/2023-10-22-trunk-or-treat.pdf" target="_blank">
+      <strong>Trunk or Treat</strong>  
     </a>
-    welcoming Bishop Emeritus Murray Finck after service.
+  </div>
+  <div class="event-time">
+    5 PM
   </div>
 </div>
 HTML
     ),
-    3 => array(
-      'date' => '2023-10-01',
+    9 => array(
+      'date' => '2023-10-24',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <a href="event-fliers/2023-10-10-study-buddies.pdf" target="_blank">
+      <strong>Study Buddies</strong>  
+    </a>
+  </div>
+  <div class="event-time">
+    3 PM
+  </div>
+</div>
+HTML
+    ),
+    10 => array(
+      'date' => '2023-10-25',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <a href="event-fliers/2023-10-11-bible-study.pdf" target="_blank">
+      <strong>Bible Study</strong>  
+    </a>
+  </div>
+  <div class="event-time">
+    9 AM
+  </div>
+</div>
+HTML
+    ),
+    11 => array(
+      'date' => '2023-10-26',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <a href="event-fliers/2023-10-10-study-buddies.pdf" target="_blank">
+      <strong>Study Buddies</strong>  
+    </a>
+  </div>
+  <div class="event-time">
+    3 PM
+  </div>
+</div>
+HTML
+    ),
+    12 => array(
+      'date' => '2023-10-29',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
     Worship Service
+    <br />
+    <strong>Reformation Sunday</strong>
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+HTML
+    ),
+    13 => array(
+      'date' => '2023-11-01',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <a href="event-fliers/2023-10-11-bible-study.pdf" target="_blank">
+      <strong>Bible Study</strong>  
+    </a>
+  </div>
+  <div class="event-time">
+    9 AM
+  </div>
+</div>
+HTML
+    ),
+    14 => array(
+      'date' => '2023-11-02',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <a href="event-fliers/2023-10-10-study-buddies.pdf" target="_blank">
+      <strong>Study Buddies</strong>  
+    </a>
+  </div>
+  <div class="event-time">
+    3 PM
+  </div>
+</div>
+HTML
+    ),
+    15 => array(
+      'date' => '2023-11-05',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    Worship Service
+    <br />
+    <strong>All Saints&rsquo; Sunday</strong>
   </div>
   <div class="event-time">
     10 AM
