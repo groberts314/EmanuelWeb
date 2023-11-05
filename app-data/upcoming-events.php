a74f3e5abdde1965a556efe0d0ca8e1554957ef3
Geoffrey Roberts <groberts314@yahoo.com>
2023-11-05 05:46:34 -0800

    feat: updates for November 2023
    * Homepage Upcoming Events
    * Newsletter
    * Calendar

--- app-data/upcoming-events.php
+++ app-data/upcoming-events.php
@@ -1,52 +1,22 @@
  <?php
   return array(
     0 => array(
-      'date' => '2023-10-10',
+      'date' => '2023-11-05',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    <a href="event-fliers/2023-10-10-study-buddies.pdf" target="_blank">
-      <strong>Study Buddies</strong>  
-    </a>
+    Worship Service
+    <br />
+    <strong>All Saints&rsquo; Sunday</strong>
   </div>
   <div class="event-time">
-    3 PM
+    10 AM
   </div>
 </div>
 HTML
     ),
     1 => array(
-      'date' => '2023-10-11',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <a href="event-fliers/2023-10-11-bible-study.pdf" target="_blank">
-      <strong>Bible Study</strong>  
-    </a>
-  </div>
-  <div class="event-time">
-    9 AM
-  </div>
-</div>
-HTML
-    ),
-    2 => array(
-      'date' => '2023-10-12',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <a href="event-fliers/2023-10-10-study-buddies.pdf" target="_blank">
-      <strong>Study Buddies</strong>  
-    </a>
-  </div>
-  <div class="event-time">
-    3 PM
-  </div>
-</div>
-HTML
-    ),
-    3 => array(
-      'date' => '2023-10-15',
+      'date' => '2023-11-12',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
@@ -58,8 +28,45 @@ HTML
 </div>
 HTML
     ),
+    2 => array(
+      'date' => '2023-11-17',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <a href="event-fliers/2023-11-17-ladies-lunch.pdf" target="_blank">
+      <strong>What&rsquo;s 4 Lunch Ladies?</strong>
+    </a>
+  </div>
+  <div class="event-time">
+    11:30 AM
+  </div>
+</div>
+HTML
+    ),
+    3 => array(
+      'date' => '2023-11-19',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    Worship Service
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <a href="event-fliers/2023-11-19-food-donations.pdf" target="_blank">
+      <strong>2023 Annual Thanksgiving Food Donation</strong>
+    </a>
+    <br />
+    Donations due at First Lutheran Church
+  </div>
+</div>
+HTML
+    ),
     4 => array(
-      'date' => '2023-10-16',
+      'date' => '2023-11-20',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
@@ -74,174 +81,66 @@ HTML
 HTML
     ),
     5 => array(
-      'date' => '2023-10-17',
+      'date' => '2023-11-26',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    <a href="event-fliers/2023-10-10-study-buddies.pdf" target="_blank">
-      <strong>Study Buddies</strong>  
-    </a>
+    Worship Service
   </div>
   <div class="event-time">
-    3 PM
+    10 AM
   </div>
 </div>
 HTML
     ),
     6 => array(
-      'date' => '2023-10-18',
+      'date' => '2023-12-03',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    <a href="event-fliers/2023-10-11-bible-study.pdf" target="_blank">
-      <strong>Bible Study</strong>  
-    </a>
+    Worship Service
+    <br />
+    <strong>First Sunday in Advent</strong>
   </div>
   <div class="event-time">
-    9 AM
+    10 AM
   </div>
 </div>
 HTML
     ),
     7 => array(
-      'date' => '2023-10-19',
+      'date' => '2023-12-10',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    <a href="event-fliers/2023-10-10-study-buddies.pdf" target="_blank">
-      <strong>Study Buddies</strong>  
-    </a>
+    Worship Service
+    <br />
+    <strong>Second Sunday in Advent</strong>
   </div>
   <div class="event-time">
-    3 PM
+    10 AM
   </div>
 </div>
 HTML
     ),
     8 => array(
-      'date' => '2023-10-22',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    Worship Service
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <a href="event-fliers/2023-10-22-trunk-or-treat.jpg" target="_blank">
-      <strong>Trunk or Treat</strong>  
-    </a>
-  </div>
-  <div class="event-time">
-    5 PM
-  </div>
-</div>
-HTML
-    ),
-    9 => array(
-      'date' => '2023-10-24',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <a href="event-fliers/2023-10-10-study-buddies.pdf" target="_blank">
-      <strong>Study Buddies</strong>  
-    </a>
-  </div>
-  <div class="event-time">
-    3 PM
-  </div>
-</div>
-HTML
-    ),
-    10 => array(
-      'date' => '2023-10-25',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <a href="event-fliers/2023-10-11-bible-study.pdf" target="_blank">
-      <strong>Bible Study</strong>  
-    </a>
-  </div>
-  <div class="event-time">
-    9 AM
-  </div>
-</div>
-HTML
-    ),
-    11 => array(
-      'date' => '2023-10-26',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <a href="event-fliers/2023-10-10-study-buddies.pdf" target="_blank">
-      <strong>Study Buddies</strong>  
-    </a>
-  </div>
-  <div class="event-time">
-    3 PM
-  </div>
-</div>
-HTML
-    ),
-    12 => array(
-      'date' => '2023-10-29',
+      'date' => '2023-12-17',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
     Worship Service
     <br />
-    <strong>Reformation Sunday</strong>
+    <strong>Third Sunday in Advent</strong>
   </div>
   <div class="event-time">
     10 AM
   </div>
 </div>
-HTML
-    ),
-    13 => array(
-      'date' => '2023-11-01',
-      'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    <a href="event-fliers/2023-10-11-bible-study.pdf" target="_blank">
-      <strong>Bible Study</strong>  
+    <a href="event-fliers/2023-12-17-childrens-xmas-musical.pdf" target="_blank">
+        <strong>Children&rsquo;s Christmas Musical</strong>  
     </a>
-  </div>
-  <div class="event-time">
-    9 AM
-  </div>
-</div>
-HTML
-    ),
-    14 => array(
-      'date' => '2023-11-02',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <a href="event-fliers/2023-10-10-study-buddies.pdf" target="_blank">
-      <strong>Study Buddies</strong>  
-    </a>
-  </div>
-  <div class="event-time">
-    3 PM
-  </div>
-</div>
-HTML
-    ),
-    15 => array(
-      'date' => '2023-11-05',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    Worship Service
-    <br />
-    <strong>All Saints&rsquo; Sunday</strong>
-  </div>
-  <div class="event-time">
-    10 AM
   </div>
 </div>
 HTML
--- newsletter.php
+++ newsletter.php
@@ -18,16 +18,16 @@
           <div class="newsletter-issue-list">
             <h2 class="section-title no-top-margin">Current Issue</h2>
             <p>
-              <a href="../newsletters/newsletter-2023-10.pdf" target="_blank">October 2023</a>
+              <a href="../newsletters/newsletter-2023-11.pdf" target="_blank">November 2023</a>
             </p>
           </div>
           <div class="newsletter-current-issue-thumbnail-mobile hidden-md hidden-lg">
-            <img src="images/newsletter/newsletter-2023-10.jpg" alt="The Good News: October 2023" />
+            <img src="images/newsletter/newsletter-2023-11.jpg" alt="The Good News: November 2023" />
           </div>
         </div>
       </div>
       <div class="col-xs-12 col-md-4 photo-right no-btm-margin hidden-xs hidden-sm">
-        <img src="images/newsletter/newsletter-2023-10.jpg" alt="The Good News: October 2023" />
+        <img src="images/newsletter/newsletter-2023-11.jpg" alt="The Good News: November 2023" />
       </div>
       <div class="col-xs-12">
         <div class="newsletter-additional-issues-list">
@@ -45,6 +45,9 @@
                 <div class="panel-body">
                   <div class="newsletter-year-issue-list">
                     <ul class="list-group">
+                      <li class="list-group-item" data-thumbnail="images/newsletter/newsletter-2023-10.jpg" data-target="#thumbnail-2023">
+                        <a href="../newsletters/newsletter-2023-10.pdf" target="_blank">October 2023</a>
+                      </li>
                       <li class="list-group-item" data-thumbnail="images/newsletter/newsletter-2023-09.jpg" data-target="#thumbnail-2023">
                         <a href="../newsletters/newsletter-2023-09.pdf" target="_blank">September 2023</a>
                       </li>
@@ -75,7 +78,7 @@
                     </ul>
                   </div>
                   <div class="newsletter-year-issue-thumbnail">
-                    <img id="thumbnail-2023" src="images/newsletter/newsletter-2023-09.jpg" alt="The Good News: 2023" />
+                    <img id="thumbnail-2023" src="images/newsletter/newsletter-2023-10.jpg" alt="The Good News: 2023" />
                   </div>
                 </div>
               </div>
