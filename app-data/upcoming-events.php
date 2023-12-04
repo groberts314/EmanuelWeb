1e14587d48a575bd72541773c4005f8b37dbb2ab
Geoffrey Roberts <groberts314@yahoo.com>
2023-12-04 08:59:15 -0800

    feat: update Upcoming Events for December 2023 (at least partially)

--- app-data/upcoming-events.php
+++ app-data/upcoming-events.php
@@ -1,13 +1,13 @@
  <?php
   return array(
     0 => array(
-      'date' => '2023-11-05',
+      'date' => '2023-12-10',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
     Worship Service
     <br />
-    <strong>All Saints&rsquo; Sunday</strong>
+    <strong>Second Sunday in Advent / Lessons &amp; Carols Service</strong>
   </div>
   <div class="event-time">
     10 AM
@@ -16,114 +16,6 @@
 HTML
     ),
     1 => array(
-      'date' => '2023-11-12',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    Worship Service
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-HTML
-    ),
-    2 => array(
-      'date' => '2023-11-17',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <a href="event-fliers/2023-11-17-ladies-lunch.pdf" target="_blank">
-      <strong>What&rsquo;s 4 Lunch Ladies?</strong>
-    </a>
-  </div>
-  <div class="event-time">
-    11:30 AM
-  </div>
-</div>
-HTML
-    ),
-    3 => array(
-      'date' => '2023-11-19',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    Worship Service
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <a href="event-fliers/2023-11-19-food-donations.pdf" target="_blank">
-      <strong>2023 Annual Thanksgiving Food Donation</strong>
-    </a>
-    <br />
-    Donations due at First Lutheran Church
-  </div>
-</div>
-HTML
-    ),
-    4 => array(
-      'date' => '2023-11-20',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <a href="event-fliers/2023-10-16-joy-circle.pdf" target="_blank">
-      <strong>Joy Circle Women&rsquo;s Bible Study</strong>  
-    </a>
-  </div>
-  <div class="event-time">
-    9:30 AM
-  </div>
-</div>
-HTML
-    ),
-    5 => array(
-      'date' => '2023-11-26',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    Worship Service
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-HTML
-    ),
-    6 => array(
-      'date' => '2023-12-03',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    Worship Service
-    <br />
-    <strong>First Sunday in Advent</strong>
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-HTML
-    ),
-    7 => array(
-      'date' => '2023-12-10',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    Worship Service
-    <br />
-    <strong>Second Sunday in Advent</strong>
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-HTML
-    ),
-    8 => array(
       'date' => '2023-12-17',
       'content' => <<<HTML
 <div class="event-detail">
@@ -144,6 +36,42 @@ HTML
   </div>
 </div>
 HTML
+    ),
+    2 => array(
+      'date' => '2023-12-24',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    Worship Service
+    <br />
+    <strong>Fourth Sunday in Advent</strong>
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+<div class="event-detail">
+<div class="event-description">
+    <strong>Christmas Eve Worship</strong>
+  </div>
+  <div class="event-time">
+    7 PM
+  </div>
+</div>
+HTML      
+    ),
+    3 => array(
+      'date' => '2023-12-31',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    Worship Service
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+HTML      
     )
   );
 ?>
