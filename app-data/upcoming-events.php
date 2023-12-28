23da43deb5c9e88b928cc27b54352ba2631a5fa6
Geoffrey Roberts <groberts314@yahoo.com>
2023-12-28 08:38:59 -0800

    feat: various updates for last week of December 2023
    * Readings for December 24, 2023 (4th Sunday of Advent + Christmas Eve)
    * Add Worship on Sunday, January 7, 2024 to Upcoming Events
    * Add notice under the homepage worship video directing visitors to the Special Events page for the Christmas Eve worship video

--- app-data/upcoming-events.php
+++ app-data/upcoming-events.php
@@ -1,66 +1,6 @@
  <?php
   return array(
     0 => array(
-      'date' => '2023-12-10',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    Worship Service
-    <br />
-    <strong>Second Sunday in Advent / <a href="event-fliers/2023-12-10-lessons-and-carols-worship.jpg" target="_blank">Lessons &amp; Carols Service</a></strong>
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-HTML
-    ),
-    1 => array(
-      'date' => '2023-12-17',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    Worship Service
-    <br />
-    <strong>Third Sunday in Advent</strong>
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <a href="event-fliers/2023-12-17-childrens-xmas-musical.pdf" target="_blank">
-        <strong>Children&rsquo;s Christmas Musical</strong>  
-    </a>
-  </div>
-</div>
-HTML
-    ),
-    2 => array(
-      'date' => '2023-12-24',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    Worship Service
-    <br />
-    <strong>Fourth Sunday in Advent</strong>
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-<div class="event-detail">
-<div class="event-description">
-    <strong>Christmas Eve Worship</strong>
-  </div>
-  <div class="event-time">
-    7 PM
-  </div>
-</div>
-HTML      
-    ),
-    3 => array(
       'date' => '2023-12-31',
       'content' => <<<HTML
 <div class="event-detail">
@@ -72,6 +12,19 @@ HTML
   </div>
 </div>
 HTML      
+    ),
+    1 => array(
+      'date' => '2024-01-07',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    Worship Service
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+HTML
     )
-  );
+  )
 ?>
