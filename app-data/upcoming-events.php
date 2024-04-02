faec2968bbf8f9d61636eef27d32ca35536697b3
Geoffrey Roberts <groberts314@yahoo.com>
2024-04-02 08:54:37 -0700

    feat: updates for April 2024
    * Homepage Upcoming Events
    * Calendar
    * Newsletter

--- app-data/upcoming-events.php
+++ app-data/upcoming-events.php
@@ -1,7 +1,21 @@
  <?php
   return array(
     0 => array(
-      'date' => '2024-03-03',
+      'date' => '2024-04-06',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Men&rsquo;s Breakfast</strong><br />
+    Arthur&rsquo;s Coffee Shop
+  </div>
+  <div class="event-time">
+    8 AM
+  </div>
+</div>
+HTML
+    ),
+    1 => array(
+      'date' => '2024-04-07',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
@@ -12,23 +26,10 @@
   </div>
 </div>
 HTML
-  ),
-  1 => array(
-    'date' => '2024-03-06',
-    'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    Lenten Midweek Worship Service
-  </div>
-  <div class="event-time">
-    7 PM
-  </div>
-</div>
-HTML
-  ),
-  2 => array(
-    'date' => '2024-03-10',
-    'content' => <<<HTML
+    ),
+    2 => array(
+      'date' => '2024-04-14',
+      'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
     Worship Service
@@ -38,23 +39,10 @@ HTML
   </div>
 </div>
 HTML
-  ),
-  3 => array(
-    'date' => '2024-03-13',
-    'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    Lenten Midweek Worship Service
-  </div>
-  <div class="event-time">
-    7 PM
-  </div>
-</div>
-HTML
-  ),
-  4 => array(
-    'date' => '2024-03-17',
-    'content' => <<<HTML
+    ),
+    3 => array(
+      'date' => '2024-04-21',
+      'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
     Worship Service
@@ -64,82 +52,23 @@ HTML
   </div>
 </div>
 HTML
-  ),
-  5 => array(
-    'date' => '2024-03-18',
-    'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    Joy Circle Women&rsquo;s Bible Study
+    ),
+    4 => array(
+      'date' => '2024-04-22',
+      'content' => <<<HTML
+  <div class="event-detail">
+    <div class="event-description">
+      Joy Circle Women&rsquo;s Bible Study
+    </div>
+    <div class="event-time">
+      9:30 AM
+    </div>
   </div>
-  <div class="event-time">
-    9:30 AM
-  </div>
-</div>
-HTML
-  ),
-  6 => array(
-    'date' => '2024-03-20',
-    'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    Lenten Midweek Worship Service
-  </div>
-  <div class="event-time">
-    7 PM
-  </div>
-</div>
-HTML
-  ),
-  7 => array(
-    'date' => '2024-03-24',
-    'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Palm Sunday</strong>
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    Worship Service
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-HTML    
-  ),
-  8 => array(
-    'date' => '2024-03-28',
-    'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Maundy Thursday</strong>
-  </div>
-</div>
-HTML
-  ),
-  9 => array(
-    'date' => '2024-03-29',
-    'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Good Friday</strong>
-  </div>
-</div>
-HTML
-  ),
-  10 => array(
-    'date' => '2024-03-31',
-    'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>
-      Easter Sunday<br />
-      HE Is Risen Indeed!  Alleluia!
-    </strong>
-  </div>
-</div>
+  HTML
+    ),
+    5 => array(
+      'date' => '2024-04-28',
+      'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
     Worship Service
@@ -149,10 +78,10 @@ HTML
   </div>
 </div>
 HTML
-  ),
-  11 => array(
-    'date' => '2024-04-07',
-    'content' => <<<HTML
+    ),
+    6 => array(
+      'date' => '2024-05-05',
+      'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
     Worship Service
@@ -162,6 +91,6 @@ HTML
   </div>
 </div>
 HTML
+    )
   )
-)
 ?>
