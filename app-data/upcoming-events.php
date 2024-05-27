941e86e728976d4c5b7f1e16af00f51031b2fd1e
Geoffrey Roberts <groberts314@yahoo.com>
2024-05-27 07:31:49 -0700

    feat: Upcoming Events for June 2024

--- app-data/upcoming-events.php
+++ app-data/upcoming-events.php
@@ -1,12 +1,12 @@
  <?php
   return array(
     0 => array(
-      'date' => '2024-05-04',
+      'date' => '2024-06-01',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    <strong>Men&rsquo;s Breakfast</strong><br />
-    Arthur&rsquo;s Coffee Shop
+    <strong>Men&rsquo;s Fellowship Breakfast</strong><br />
+    Arthur&rsquo;s Coffe Shop
   </div>
   <div class="event-time">
     8 AM
@@ -15,7 +15,7 @@
 HTML
     ),
     1 => array(
-      'date' => '2024-05-05',
+      'date' => '2024-06-02',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
@@ -25,10 +25,18 @@ HTML
     10 AM
   </div>
 </div>
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Pacific Symphony Orchestra in Concert</strong>
+  </div>
+  <div class="event-time">
+    5 PM
+  </div>
+</div>
 HTML
     ),
     2 => array(
-      'date' => '2024-05-12',
+      'date' => '2024-06-09',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
@@ -41,7 +49,7 @@ HTML
 HTML
     ),
     3 => array(
-      'date' => '2024-05-19',
+      'date' => '2024-06-16',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
@@ -53,8 +61,21 @@ HTML
 </div>
 HTML
     ),
+    4 => array(
+      'date' => '2024-06-17',
+      'content' => <<<HTML
+  <div class="event-detail">
+    <div class="event-description">
+      <strong>Joy Circle Women&rsquo;s Bible Study</strong>
+    </div>
+    <div class="event-time">
+      9:30 AM
+    </div>
+  </div>
+HTML
+    ),
     5 => array(
-      'date' => '2024-05-26',
+      'date' => '2024-06-23',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
@@ -67,7 +88,7 @@ HTML
 HTML
     ),
     6 => array(
-      'date' => '2024-06-02',
+      'date' => '2024-06-30',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
