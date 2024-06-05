904d39a2c0a9876f4dd59600a9281c10fe936696
Geoffrey Roberts <groberts314@yahoo.com>
2024-06-05 03:46:14 -0700

    feat: updates for June 2024
    * Homepage Upcoming Events
    * Calendar
    * Newsletter

--- app-data/upcoming-events.php
+++ app-data/upcoming-events.php
@@ -1,41 +1,6 @@
  <?php
   return array(
     0 => array(
-      'date' => '2024-06-01',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Men&rsquo;s Fellowship Breakfast</strong><br />
-    Arthur&rsquo;s Coffe Shop
-  </div>
-  <div class="event-time">
-    8 AM
-  </div>
-</div>
-HTML
-    ),
-    1 => array(
-      'date' => '2024-06-02',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Worship Service</strong>
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Pacific Symphony Orchestra in Concert</strong>
-  </div>
-  <div class="event-time">
-    5 PM
-  </div>
-</div>
-HTML
-    ),
-    2 => array(
       'date' => '2024-06-09',
       'content' => <<<HTML
 <div class="event-detail">
@@ -48,7 +13,20 @@ HTML
 </div>
 HTML
     ),
-    3 => array(
+    1 => array(
+      'date' => '2024-06-15',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Joe Maldonado in Concert</strong>
+  </div>
+  <div class="event-time">
+    6 PM
+  </div>
+</div>
+HTML
+    ),
+    2 => array(
       'date' => '2024-06-16',
       'content' => <<<HTML
 <div class="event-detail">
@@ -61,7 +39,7 @@ HTML
 </div>
 HTML
     ),
-    4 => array(
+    3 => array(
       'date' => '2024-06-17',
       'content' => <<<HTML
   <div class="event-detail">
@@ -74,7 +52,7 @@ HTML
   </div>
 HTML
     ),
-    5 => array(
+    4 => array(
       'date' => '2024-06-23',
       'content' => <<<HTML
 <div class="event-detail">
@@ -87,8 +65,21 @@ HTML
 </div>
 HTML
     ),
-    6 => array(
+    5 => array(
       'date' => '2024-06-30',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Worship Service</strong>
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+HTML
+    ),
+    6 => array(
+      'date' => '2024-07-07',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
