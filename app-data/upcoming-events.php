c70f44b6d3b4802c00e2e0fd5472be93fbe6a540
Geoffrey Roberts <groberts314@yahoo.com>
2024-06-30 03:49:14 -0700

    feat: updates for July 2024
    * Homepage Upcoming Events
    * Calendar
    * Newsletter

--- app-data/upcoming-events.php
+++ app-data/upcoming-events.php
@@ -1,71 +1,6 @@
  <?php
   return array(
     0 => array(
-      'date' => '2024-06-09',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Worship Service</strong>
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-HTML
-    ),
-    1 => array(
-      'date' => '2024-06-15',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Joe Maldonado in Concert</strong>
-  </div>
-  <div class="event-time">
-    6 PM
-  </div>
-</div>
-HTML
-    ),
-    2 => array(
-      'date' => '2024-06-16',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Worship Service</strong>
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-HTML
-    ),
-    3 => array(
-      'date' => '2024-06-17',
-      'content' => <<<HTML
-  <div class="event-detail">
-    <div class="event-description">
-      <strong>Joy Circle Women&rsquo;s Bible Study</strong>
-    </div>
-    <div class="event-time">
-      9:30 AM
-    </div>
-  </div>
-HTML
-    ),
-    4 => array(
-      'date' => '2024-06-23',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Worship Service</strong>
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-HTML
-    ),
-    5 => array(
       'date' => '2024-06-30',
       'content' => <<<HTML
 <div class="event-detail">
@@ -78,8 +13,99 @@ HTML
 </div>
 HTML
     ),
-    6 => array(
+    1 => array(
+      'date' => '2024-07-06',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Men&rsquo;s Breakfast</strong>
+  </div>
+  <div class="event-time">
+    8 AM
+  </div>
+</div>
+HTML
+    ),
+    2 => array(
       'date' => '2024-07-07',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Worship Service</strong>
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+HTML
+    ),
+    3 => array(
+      'date' => '2024-07-14',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Worship Service</strong>
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+HTML
+    ),
+    4 => array(
+      'date' => '2024-07-15',
+      'content' => <<<HTML
+  <div class="event-detail">
+    <div class="event-description">
+      <strong>Joy Circle Women&rsquo;s Bible Study</strong>
+    </div>
+    <div class="event-time">
+      9:30 AM
+    </div>
+  </div>
+HTML
+    ),
+    5 => array(
+      'date' => '2024-07-19',
+      'content' => <<<HTML
+  <div class="event-detail">
+    <div class="event-description">
+      <strong>What&rsquo;s 4 Lunch Ladies</strong>
+    </div>
+    <div class="event-time">
+      11:30 AM
+    </div>
+  </div>
+HTML
+    ),
+    6 => array(
+      'date' => '2024-07-21',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Worship Service</strong>
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+HTML
+    ),
+    7 => array(
+      'date' => '2024-07-28',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Worship Service</strong>
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+HTML
+    ),
+    8 => array(
+      'date' => '2024-08-04',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
--- newsletter.php
+++ newsletter.php
@@ -18,16 +18,16 @@
           <div class="newsletter-issue-list">
             <h2 class="section-title no-top-margin">Current Issue</h2>
             <p>
-              <a href="../newsletters/newsletter-2024-06.pdf" target="_blank">June 2024</a>
+              <a href="../newsletters/newsletter-2024-07.pdf" target="_blank">July 2024</a>
             </p>
           </div>
           <div class="newsletter-current-issue-thumbnail-mobile hidden-md hidden-lg">
-            <img src="images/newsletter/newsletter-2024-06.jpg" alt="The Good News: June 2024" />
+            <img src="images/newsletter/newsletter-2024-07.jpg" alt="The Good News: July 2024" />
           </div>
         </div>
       </div>
       <div class="col-xs-12 col-md-4 photo-right no-btm-margin hidden-xs hidden-sm">
-        <img src="images/newsletter/newsletter-2024-06.jpg" alt="The Good News: June 2024" />
+        <img src="images/newsletter/newsletter-2024-07.jpg" alt="The Good News: July 2024" />
       </div>
       <div class="col-xs-12">
         <div class="newsletter-additional-issues-list">
@@ -45,6 +45,9 @@
                 <div class="panel-body">
                   <div class="newsletter-year-issue-list">
                     <ul class="list-group">
+                      <li class="list-group-item" data-thumbnail="images/newsletter/newsletter-2024-06.jpg" data-target="#thumbnail-2024">
+                        <a href="../newsletters/newsletter-2024-06.pdf" target="_blank">June 2024</a>
+                      </li>
                       <li class="list-group-item" data-thumbnail="images/newsletter/newsletter-2024-05.jpg" data-target="#thumbnail-2024">
                         <a href="../newsletters/newsletter-2024-05.pdf" target="_blank">May 2024</a>
                       </li>
@@ -63,7 +66,7 @@
                     </ul>
                   </div>
                   <div class="newsletter-year-issue-thumbnail">
-                    <img id="thumbnail-2024" src="images/newsletter/newsletter-2024-05.jpg" alt="The Good News: 2024" />
+                    <img id="thumbnail-2024" src="images/newsletter/newsletter-2024-06.jpg" alt="The Good News: 2024" />
                   </div>
                 </div>
               </div>
