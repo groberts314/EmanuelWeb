49ade354fafafb69800bc61e70cc50c57e44987c
Geoffrey Roberts <groberts314@yahoo.com>
2024-07-28 06:02:45 -0700

    feat: updates for August 2024
    * Homepage Upcoming Events
    * Calendar
    * Newsletter

--- calendar.php
+++ calendar.php
@@ -15,8 +15,21 @@
       <div class="col-xs-12">
         <h1 class="page-title">Event Calendar</h1>
         <p>
-          <? /* <a href="./calendars/calendar-2022-03.pdf" target="_blank">March 2022 printer-friendly calendar</a><br /> */?>
-          <a href="./calendars/calendar-2024-07.pdf" target="_blank">July 2024 printer-friendly calendar</a>
+          <?php
+            $pacificTimeZone = new DateTimeZone('America/Los_Angeles');
+            $now = new DateTime("now", $pacificTimeZone);
+            $beginningOfNextMonth = new DateTime('2024-08-01', $pacificTimeZone);
+
+            if ($now > $beginningOfNextMonth) {
+              echo <<<HTML
+              <a href="./calendars/calendar-2024-08.pdf" target="_blank">August 2024 printer-friendly calendar</a>
+HTML;
+            } else {
+              echo <<<HTML
+              <a href="./calendars/calendar-2024-07.pdf" target="_blank">July 2024 printer-friendly calendar</a>
+HTML;
+            }
+          ?>
         </p>
         <? /* Embedded calendar from Breeze ChMS seems to be blank; it has no events on it */?>
         <?/* <iframe src="https://emanuellutheran.breezechms.com/embed/calendar/grid?size=medium&color=gray&calendars=srg9ErllnQBH%2BDD4NjLNQb4cUJHWej5sy5FLrtsbUc4x%2FLSC%2Fo4x2MFxxOZAJ4jli3mdvTLaVJQ5UvZ8ZI%2B31Q%3D%3D"
@@ -28,7 +41,17 @@
                 style="border-width: 0px;">
         </iframe> */?>
         <? /* For now, just show an image of the PDF calendar */?>
-        <img src="images/calendars/calendar-2024-07.jpg" alt="July 2024 Event Calendar" />
+        <?php
+          if ($now > $beginningOfNextMonth) {
+            echo <<<HTML
+            <img src="images/calendars/calendar-2024-08.jpg" alt="August 2024 Event Calendar" />
+HTML;
+          } else {
+            echo <<<HTML
+            <img src="images/calendars/calendar-2024-07.jpg" alt="July 2024 Event Calendar" />
+HTML;
+          }
+        ?>
       </div>
     </div>
 
