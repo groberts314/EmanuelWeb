88c8c84780cb1084065cb007bb54d0aa5311ff87
Geoffrey Roberts <groberts314@yahoo.com>
2025-05-03 14:00:51 -0700

    feat: updates for May 2025
    * Homepage Upcoming Events
    * Calendar
    * Newsletter

--- app-data/upcoming-events.php
+++ app-data/upcoming-events.php
@@ -1,140 +1,6 @@
  <?php
   return array(
     0 => array(
-      'date' => '2025-04-06',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Worship Service</strong>
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-HTML
-    ),
-    1 => array(
-      'date' => '2025-04-09',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Midweek Lenten Worship</strong>
-    <br />
-    On Zoom
-  </div>
-  <div class="event-time">
-    7 PM
-  </div>
-</div>
-HTML
-    ),
-    2 => array(
-      'date' => '2025-04-13',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Palm Sunday</strong>
-    <br />
-    Worship Service
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-HTML
-    ),
-    3 => array(
-      'date' => '2025-04-17',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Maundy Thursday</strong>
-    <br />
-    Evening Worship
-  </div>
-  <div class="event-time">
-    7 PM
-  </div>
-</div>
-HTML
-    ),
-    4 => array(
-      'date' => '2025-04-18',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Good Friday Worship</strong>
-    <br />
-    Evening Worship
-  </div>
-  <div class="event-time">
-    7 PM
-  </div>
-</div>
-HTML
-    ),
-    5 => array(
-      'date' => '2025-04-20',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>
-      Easter Sunday
-      <br />
-      <em>He is risen indeed, Alleluia!</em>
-    </strong>
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    Worship Service
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-HTML
-    ),
-    6 => array(
-      'date' => '2025-04-21',
-      'content' => <<<HTML
-  <div class="event-detail">
-    <div class="event-description">
-      <strong>Joy Circle Women&rsquo;s Bible Study</strong>
-    </div>
-    <div class="event-time">
-      9:30 AM
-    </div>
-  </div>
-HTML
-    ),
-    7 => array(
-      'date' => '2025-04-25',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>What&rsquo;s 4 Lunch Ladies?!</strong>
-  </div>
-  <div class="event-time">
-    11:30 AM
-  </div>
-</div>
-HTML
-    ),
-    8 => array(
-      'date' => '2025-04-27',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Worship Service</strong>
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-HTML
-    ),
-    9 => array(
       'date' => '2025-05-03',
       'content' => <<<HTML
 <div class="event-detail">
@@ -147,8 +13,112 @@ HTML
 </div>
 HTML
     ),
-    10 => array(
+    1 => array(
       'date' => '2025-05-04',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Worship Service</strong>
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+HTML
+    ),
+    2 => array(
+      'date' => '2025-05-11',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Worship Service</strong>
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+HTML
+    ),
+    3 => array(
+      'date' => '2025-05-12',
+      'content' => <<<HTML
+  <div class="event-detail">
+    <div class="event-description">
+      <strong>Joy Circle Women&rsquo;s Bible Study</strong>
+    </div>
+    <div class="event-time">
+      9:30 AM
+    </div>
+  </div>
+HTML
+    ),
+    4 => array(
+      'date' => '2025-05-18',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Worship Service</strong>
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+HTML
+    ),
+    5 => array(
+      'date' => '2025-05-23',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>What&rsquo;s 4 Lunch Ladies?!</strong>
+  </div>
+  <div class="event-time">
+    11:30 AM
+  </div>
+</div>
+HTML
+    ),
+    6 => array(
+      'date' => '2025-05-25',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Worship Service</strong>
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+HTML
+    ),
+    7 => array(
+      'date' => '2025-06-01',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Worship Service</strong>
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+HTML
+    ),
+    8 => array(
+      'date' => '2025-06-07',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Men&rsquo;s Breakfast</strong>
+  </div>
+  <div class="event-time">
+    8 AM
+  </div>
+</div>
+HTML
+    ),
+    9 => array(
+      'date' => '2025-06-08',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
