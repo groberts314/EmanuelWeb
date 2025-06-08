9e4a65c0ec338c713ad66247e6306fe36d0898c9
Geoffrey Roberts <groberts314@yahoo.com>
2025-06-08 15:17:27 -0700

    feat: Homepage - switch to YouTube embed due to streaming issues

--- index.php
+++ index.php
@@ -37,21 +37,21 @@
 
           <div class="fluid-width-video-wrapper">
             <?php /* Worship Video embedded from YouTube in "fluid width" container */ ?>
-            <?php /*<iframe class="video-iframe" src="https://www.youtube-nocookie.com/embed/fpG8BLY_83M" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>*/ ?>
+            <iframe class="video-iframe" src="https://www.youtube-nocookie.com/embed/8CwEMtqGv1g" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
             <?php /* Vimeo Live Stream (Recurring Event - Original one) */ ?>
             <?php /* <iframe class="video-iframe" src="https://vimeo.com/event/812403/embed" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe> */ ?>
             <?php /* Vimeo Live Stream (Recurring Event - New One for 9 AM Traditional Worship) */ ?>
-            <iframe class="video-iframe" src="https://vimeo.com/event/1277638/embed" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+            <?php /*<iframe class="video-iframe" src="https://vimeo.com/event/1277638/embed" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>*/ ?>
 
             <?php /* Vimeo Live Stream (Specific Video) */ ?>
             <?php /*<iframe class="video-iframe" src="https://player.vimeo.com/video/594210586" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>*/ ?>
           </div>
 
-          <p style="font-weight: bold; margin-top: 22px; text-align: center">
+          <?php /*<p style="font-weight: bold; margin-top: 22px; text-align: center">
             We have been having some difficulties with our livestream.<br />
             Sunday, June 1, 2025 Worship video available on <a href="https://www.youtube.com/watch?v=2Oc-VLQSmL8" target="_blank">YouTube</a>.
-          </p>
+          </p> */ ?>
       </div>
       <div class="module-container worship-audio"> <?php /* TODO: Replace class `worship-audio` with something more appropriate */ ?>
           <?php require_once('./partials/homepage-v2-bible-readings.phtml') ?>
