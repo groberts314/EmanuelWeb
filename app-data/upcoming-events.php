4deeae1c843ae217ae3406e9b03155a144a23b71
Geoffrey Roberts <groberts314@yahoo.com>
2025-11-30 07:06:15 -0800

    feat: updates for December 2025
    * Homepage Upcoming Events
    * Calendar
    * Newsletter

--- app-data/upcoming-events.php
+++ app-data/upcoming-events.php
@@ -1,27 +1,13 @@
  <?php
   return array(
     0 => array(
-      'date' => '2025-11-01',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Men&rsquo; Breakfast</strong>
-  </div>
-  <div class="event-time">
-    8 AM
-  </div>
-</div>
-HTML
-    ),
-    1 => array(
-      'date' => '2025-11-02',
+      'date' => '2025-11-30',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
     <strong>
-      Worship Service
-      <br />
-      All Saints&rsquo; Sunday
+      Worship Service<br />
+      1st Sunday in Advent
     </strong>
   </div>
   <div class="event-time">
@@ -38,12 +24,28 @@ HTML
 </div>
 HTML
     ),
-    2 => array(
-      'date' => '2025-11-09',
+    1 => array(
+      'date' => '2025-12-06',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    <strong>Worship Service</strong>
+    <strong>Men&rsquo;s Breakfast</strong>
+  </div>
+  <div class="event-time">
+    8 AM
+  </div>
+</div>
+HTML
+    ),
+    2 => array(
+      'date' => '2025-12-07',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>
+      Worship Service<br />
+      2nd Sunday in Advent
+    </strong>
   </div>
   <div class="event-time">
     10 AM
@@ -60,81 +62,14 @@ HTML
 HTML
     ),
     3 => array(
-      'date' => '2025-11-16',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Worship Service</strong>
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Sunday School</strong>
-  </div>
-  <div class="event-time">
-    11 AM
-  </div>
-</div>
-HTML
-    ),
-    4 => array(
-      'date' => '2025-11-17',
-      'content' => <<<HTML
-  <div class="event-detail">
-    <div class="event-description">
-      <strong>Joy Circle Women&rsquo;s Bible Study</strong>
-    </div>
-    <div class="event-time">
-      9:30 AM
-    </div>
-  </div>
-HTML
-    ),
-    5 => array(
-      'date' => '2025-11-21',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>What&rsquo;s 4 Lunch Ladies?!</strong>
-  </div>
-  <div class="event-time">
-    11:30 AM
-  </div>
-</div>
-HTML
-    ),
-    6 => array(
-      'date' => '2025-11-23',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Worship Service</strong>
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Sunday School</strong>
-  </div>
-  <div class="event-time">
-    11 AM
-  </div>
-</div>
-HTML
-    ),
-    7 => array(
-      'date' => '2025-11-30',
+      'date' => '2025-12-14',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
     <strong>
       Worship Service<br />
-      First Sunday in Advent
+      3rd Sunday in Advent<br />
+      <em>Lessons &amp; Carols</em>
     </strong>
   </div>
   <div class="event-time">
@@ -151,12 +86,99 @@ HTML
 </div>
 HTML
     ),
-    8 => array(
-      'date' => '2025-12-06',
+    4 => array(
+      'date' => '2025-12-19',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    <strong>Men&rsquo; Breakfast</strong>
+    <strong>What&rsquo;s 4 Lunch Ladies?!</strong>
+  </div>
+  <div class="event-time">
+    11:30 AM
+  </div>
+</div>
+HTML
+    ),
+    5 => array(
+      'date' => '2025-12-21',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>
+      Worship Service<br />
+      4th Sunday in Advent<br />
+      <em>Children&rsquo;s Program</em>
+    </strong>
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Sunday School</strong>
+  </div>
+  <div class="event-time">
+    11 AM
+  </div>
+</div>
+HTML
+    ),
+    6 => array(
+      'date' => '2025-12-22',
+      'content' => <<<HTML
+  <div class="event-detail">
+    <div class="event-description">
+      <strong>Joy Circle Women&rsquo;s Bible Study</strong>
+    </div>
+    <div class="event-time">
+      9:30 AM
+    </div>
+  </div>
+HTML
+    ),
+    7 => array(
+      'date' => '2025-12-24',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Chrismas Eve Candlelight Worship</strong>
+  </div>
+  <div class="event-time">
+    7 PM
+  </div>
+</div>
+HTML
+    ),
+    8 => array(
+      'date' => '2025-12-28',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>
+      Worship Service
+    </strong>
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Sunday School</strong>
+  </div>
+  <div class="event-time">
+    11 AM
+  </div>
+</div>
+HTML
+    ),
+    9 => array(
+      'date' => '2026-01-03',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Men&rsquo;s Breakfast</strong>
   </div>
   <div class="event-time">
     8 AM
@@ -164,14 +186,13 @@ HTML
 </div>
 HTML
     ),
-    9 => array(
-      'date' => '2025-12-07',
+    10 => array(
+      'date' => '2026-01-04',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
     <strong>
-      Worship Service<br />
-      Second Sunday in Advent
+      Worship Service
     </strong>
   </div>
   <div class="event-time">
