5bb5eeee46ee880aff7dbd776fa38932bd255513
Geoffrey Roberts <groberts314@yahoo.com>
2026-04-02 06:21:27 -0700

    feat: updates for April 2026
    * Homepage Upcoming Events
    * Calendar
    * Newsletter

--- app-data/upcoming-events.php
+++ app-data/upcoming-events.php
@@ -1,34 +1,24 @@
  <?php
   return array(
     0 => array(
-      'date' => '2026-03-01',
+      'date' => '2026-04-02',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    <strong>
-      Worship Service
-    </strong>
+    <strong>Maundy Thursday Worship Service</strong>
   </div>
   <div class="event-time">
-    10 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Sunday School</strong>
-  </div>
-  <div class="event-time">
-    11 AM
+    7 PM
   </div>
 </div>
 HTML
     ),
     1 => array(
-      'date' => '2026-03-04',
-      'content' =>  <<<HTML
+      'date' => '2026-04-03',
+      'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    <strong>Lenten Midweek Worship (Zoom)</strong>
+    <strong>Good Friday Worship Service</strong>
   </div>
   <div class="event-time">
     7 PM
@@ -37,7 +27,7 @@ HTML
 HTML
     ),
     2 => array(
-      'date' => '2026-03-07',
+      'date' => '2026-04-04',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
@@ -50,7 +40,32 @@ HTML
 HTML
     ),
     3 => array(
-      'date' => '2026-03-08',
+      'date' => '2026-04-05',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>
+      Worship Service<br />
+      Easter Sunday<br />
+      <em>He is Risen Indeed!  Allelujah!</em>
+    </strong>
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Breakfast &amp; Egg Hunt</strong>
+  </div>
+  <div class="event-time">
+    11:15 AM
+  </div>
+</div>
+HTML
+    ),
+    4 => array(
+      'date' => '2026-04-12',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
@@ -72,21 +87,8 @@ HTML
 </div>
 HTML
     ),
-    4 => array(
-      'date' => '2026-03-11',
-      'content' =>  <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Lenten Midweek Worship (Zoom)</strong>
-  </div>
-  <div class="event-time">
-    7 PM
-  </div>
-</div>
-HTML
-    ),
     5 => array(
-      'date' => '2026-03-15',
+      'date' => '2026-04-19',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
@@ -109,7 +111,7 @@ HTML
 HTML
     ),
     6 => array(
-      'date' => '2026-03-16',
+      'date' => '2026-04-20',
       'content' => <<<HTML
   <div class="event-detail">
     <div class="event-description">
@@ -122,20 +124,20 @@ HTML
 HTML
     ),
     7 => array(
-      'date' => '2026-03-18',
-      'content' =>  <<<HTML
+      'date' => '2026-04-24',
+      'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    <strong>Lenten Midweek Worship (Zoom)</strong>
+    <strong>What&rsquo;s 4 Lunch Ladies?!</strong>
   </div>
   <div class="event-time">
-    7 PM
+    11:30 AM
   </div>
 </div>
 HTML
     ),
     8 => array(
-      'date' => '2026-03-22',
+      'date' => '2026-04-26',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
@@ -158,39 +160,25 @@ HTML
 HTML
     ),
     9 => array(
-      'date' => '2026-03-25',
-      'content' =>  <<<HTML
+      'date' => '2026-05-02',
+      'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    <strong>Lenten Midweek Worship (Zoom)</strong>
+    <strong>Men&rsquo;s Breakfast</strong>
   </div>
   <div class="event-time">
-    7 PM
+    8 AM
   </div>
 </div>
 HTML
     ),
     10 => array(
-      'date' => '2026-03-27',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>What&rsquo;s 4 Lunch Ladies?!</strong>
-  </div>
-  <div class="event-time">
-    11:30 AM
-  </div>
-</div>
-HTML
-    ),
-    11 => array(
-      'date' => '2026-03-29',
+      'date' => '2026-05-03',
       'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
     <strong>
-      Worship Service<br />
-      Palm Sunday
+      Worship Service
     </strong>
   </div>
   <div class="event-time">
@@ -203,23 +191,6 @@ HTML
   </div>
   <div class="event-time">
     11 AM
-  </div>
-</div>
-HTML
-    ),
-    12 => array(
-      'date' => '2026-04-05',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>
-      Worship Service<br />
-      Easter Sunday<br />
-      <em>He is Risen Indeed!  Allelujah!</em>
-    </strong>
-  </div>
-  <div class="event-time">
-    10 AM
   </div>
 </div>
 HTML
