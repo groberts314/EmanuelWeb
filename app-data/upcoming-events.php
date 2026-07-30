c269b26e13de880320d3dd116e666b7fc7a62e6b
Geoffrey Roberts <groberts314@yahoo.com>
2026-07-30 15:52:46 -0700

    feat: updates for August 2026
    * Homepage Upcoming Events
    * Calendar
    * Newsletter

--- app-data/upcoming-events.php
+++ app-data/upcoming-events.php
@@ -1,124 +1,6 @@
  <?php
   return array(
     0 => array(
-      'date' => '2026-07-04',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Men&rsquo;s Breakfast</strong>
-  </div>
-  <div class="event-time">
-    8 AM
-  </div>
-</div>
-HTML
-    ),
-    1 => array(
-      'date' => '2026-07-05',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>
-      Worship Service
-    </strong>
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Sunday School</strong>
-  </div>
-  <div class="event-time">
-    11 AM
-  </div>
-</div>
-HTML
-    ),
-    2 => array(
-      'date' => '2026-07-12',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>
-      Worship Service
-    </strong>
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Sunday School</strong>
-  </div>
-  <div class="event-time">
-    11 AM
-  </div>
-</div>
-HTML
-    ),
-    3 => array(
-      'date' => '2026-07-19',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>
-      Worship Service
-    </strong>
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Sunday School</strong>
-  </div>
-  <div class="event-time">
-    11 AM
-  </div>
-</div>
-HTML
-    ),
-    4 => array(
-      'date' => '2026-07-24',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>What&rsquo;s 4 Lunch Ladies?!</strong>
-  </div>
-  <div class="event-time">
-    11:30 AM
-  </div>
-</div>
-HTML
-    ),
-    5 => array(
-      'date' => '2026-07-26',
-      'content' => <<<HTML
-<div class="event-detail">
-  <div class="event-description">
-    <strong>
-      Worship Service
-    </strong>
-  </div>
-  <div class="event-time">
-    10 AM
-  </div>
-</div>
-<div class="event-detail">
-  <div class="event-description">
-    <strong>Sunday School</strong>
-  </div>
-  <div class="event-time">
-    11 AM
-  </div>
-</div>
-HTML
-    ),
-    6 => array(
       'date' => '2026-08-01',
       'content' => <<<HTML
 <div class="event-detail">
@@ -129,9 +11,17 @@ HTML
     8 AM
   </div>
 </div>
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Vacation Bible School (VBS)</strong>
+  </div>
+  <div class="event-time">
+    9 AM
+  </div>
+</div>
 HTML
     ),
-    7 => array(
+    1 => array(
       'date' => '2026-08-02',
       'content' => <<<HTML
 <div class="event-detail">
@@ -144,15 +34,108 @@ HTML
     10 AM
   </div>
 </div>
+HTML
+    ),
+    2 => array(
+      'date' => '2026-08-09',
+      'content' => <<<HTML
 <div class="event-detail">
   <div class="event-description">
-    <strong>Sunday School</strong>
+    <strong>
+      Worship Service
+    </strong>
   </div>
   <div class="event-time">
-    11 AM
+    10 AM
+  </div>
+</div>
+HTML
+    ),
+    3 => array(
+      'date' => '2026-08-16',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>
+      Worship Service
+    </strong>
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+HTML
+    ),
+    4 => array(
+      'date' => '2026-08-23',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>
+      Worship Service
+    </strong>
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+HTML
+    ),
+    5 => array(
+      'date' => '2026-08-28',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>What&rsquo;s 4 Lunch Ladies?!</strong>
+  </div>
+  <div class="event-time">
+    11:30 AM
+  </div>
+</div>
+HTML
+    ),
+    6 => array(
+      'date' => '2026-08-30',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>
+      Worship Service
+    </strong>
+  </div>
+  <div class="event-time">
+    10 AM
+  </div>
+</div>
+HTML
+    ),
+    7 => array(
+      'date' => '2026-09-05',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>Men&rsquo;s Breakfast</strong>
+  </div>
+  <div class="event-time">
+    8 AM
+  </div>
+</div>
+HTML      
+    ),
+    8 => array(
+      'date' => '2026-09-06',
+      'content' => <<<HTML
+<div class="event-detail">
+  <div class="event-description">
+    <strong>
+      Worship Service
+    </strong>
+  </div>
+  <div class="event-time">
+    10 AM
   </div>
 </div>
 HTML
     )
-  )
+  );
 ?>
